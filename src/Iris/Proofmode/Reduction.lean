d28011ce4ea0296c7b9709f39bd300805f2b6006
larsk21 <57503246+larsk21@users.noreply.github.com>
2022-04-27 12:54:57 +0200

    use custom simp attribute

--- src/Iris/Proofmode/Reduction.lean
+++ src/Iris/Proofmode/Reduction.lean
@@ -12,7 +12,6 @@ attribute [pm_simp] List.concat
 attribute [pm_simp] List.foldl1
 attribute [pm_simp] List.foldr1
 
--- TODO use `pm_simp` (available with `nightly-2022-04-26`)
-macro "pmReduce" : tactic => `(tactic| simp only [bigOp, List.concat, List.foldl1, List.foldr1])
+macro "pmReduce" : tactic => `(tactic| simp only [pm_simp])
 
 end Iris.Proofmode
