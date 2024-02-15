9dddde56eacb116131b9f4f8496578cb45f360d1
Shreyas Srinivas <shreyasss94@gmail.com>
2024-02-15 13:34:14 +0100

    Modified lakefile.lean to point package to the reintroduced src directory

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «Iris» where
-
+  srcDir := "./src/"
 
 require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
