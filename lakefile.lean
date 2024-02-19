f9b12da5ac9bdeb6d481f5f0ef1900be4398bf04
Shreyas Srinivas <shreyasss94@gmail.com>
2024-02-19 13:23:08 +0100

    Reverted lakefile.lean changes

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,11 @@
 import Lake
 open Lake DSL
 
-package «Iris» where
+package iris where
   srcDir := "./src/"
 
 require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 
 @[default_target]
-lean_lib «Iris» where
+lean_lib iris where
