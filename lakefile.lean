9155e6fa89b7b4d8ad7942f6e6897c213d67d66f
Shreyas Srinivas <shreyasss94@gmail.com>
2024-02-19 18:59:45 +0100

    Removed  from lean_lib declaration. Now the match with the previous lakefile.lean is perfect

--- lakefile.lean
+++ lakefile.lean
@@ -8,4 +8,4 @@ require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 
 @[default_target]
-lean_lib iris where
+lean_lib iris
