20799cf35446f55f1c2d16ef27c3c6308b204b1b
Mario Carneiro <di.gama@gmail.com>
2025-01-22 13:52:21 -0700

    fix build

--- lakefile.lean
+++ lakefile.lean
@@ -8,4 +8,4 @@ require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
 
 @[default_target]
-lean_lib iris
+lean_lib Iris
