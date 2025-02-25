bef3d37e310f63ff366748689aca1bb7af360add
Quang Dao <quangdao@umich.edu>
2025-02-25 18:35:38 -0500

    chore: bump to v.4.17.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package iris where
   srcDir := "./src/"
 
-require "leanprover-community" / "Qq" @ git "v4.15.0"
+require "leanprover-community" / "Qq" @ git "v4.17.0-rc1"
 
 @[default_target]
 lean_lib Iris
