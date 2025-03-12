cebf266001b404fc4690ec017b873c144c198b54
Quang Dao <quangdao@umich.edu>
2025-03-12 16:50:38 -0600

    revert version bump

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package iris where
   srcDir := "./src/"
 
-require "leanprover-community" / "Qq" @ git "v4.18.0-rc1"
+require "leanprover-community" / "Qq" @ git "v4.15.0"
 
 @[default_target]
 lean_lib Iris
