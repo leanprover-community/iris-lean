351e3a7b47cf922442fa0f52bdf44ae99c6bf09e
Сухарик <65870+suhr@users.noreply.github.com>
2025-03-30 23:58:01 +0300

    Add myself as an author

--- src/Iris/Algebra/CMRA.lean
+++ src/Iris/Algebra/CMRA.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro
+Authors: Mario Carneiro, Сухарик (@suhr)
 -/
 import Iris.Algebra.OFE
 
