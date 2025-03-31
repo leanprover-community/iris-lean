d7cae7480d4d9672f7ca0e3dde75aa479bb83ba9
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-03-31 04:15:55 -0400

    fix rebase

--- src/Iris/Algebra/CMRA.lean
+++ src/Iris/Algebra/CMRA.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Markus de Medeiros
+Authors: Mario Carneiro, Сухарик (@suhr), Markus de Medeiros
 -/
 import Iris.Algebra.OFE
 
