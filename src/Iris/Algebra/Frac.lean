4adf4cbf304299f5480b0440bd269b825ce5aa6a
Shreyas <shreyasss94@gmail.com>
2025-06-27 19:38:53 +0200

    Add name  to authors list

--- src/Iris/Algebra/Frac.lean
+++ src/Iris/Algebra/Frac.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Markus de Medeiros. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus de Medeiros
+Authors: Markus de Medeiros, Shreyas Srinivas
 -/
 
 import Iris.Algebra.CMRA
