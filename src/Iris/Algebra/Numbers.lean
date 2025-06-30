35941c2da24c605ae60a8f1544b5071626aa696d
Shreyas <shreyasss94@gmail.com>
2025-06-30 18:57:13 +0200

    Begin numbers CMRA

--- src/Iris/Algebra/Numbers.lean
+++ src/Iris/Algebra/Numbers.lean
@@ -7,16 +7,14 @@ Authors: Markus de Medeiros
 import Iris.Algebra.CMRA
 import Iris.Algebra.OFE
 
--- TODO: The typeclasses generalize the fractional construction by paramaterizing by the operations we need,
--- but this could probably be simpified, and use an actual arithmetic type if we have access to it.
--- I want this to be generalized so I can use posreal from mathlib.
-
-class One (T : Type _) where one : T
-export One(one)
+/-!
+## Numbers CMRA
+This camera provides us a resource of numbers on which a total ordering compatible with an addition operationa are defined. This module defines Numbers as a commutative additive monoid which is totally ordered. The `Numbers` class extends `CommMonoid` and `TotallyOrdered` classes and includes a list of compatbility axioms between the addition operation and total ordering relation.
+-/
 
 class CommMonoid (α : Type _) extends Add α, Zero α, One α where
-  assoc  : ∀ a b c : α, (a + b) + c = a + (b + c)
-  comm : ∀ a b : α, a + b = b + a
+  add_assoc  : ∀ a b c : α, (a + b) + c = a + (b + c)
+  add_comm : ∀ a b : α, a + b = b + a
   id_law : ∀ a : α, a + 0 = a
 
 class TotallyOrdered (α : Type _) extends LE α, LT α where
@@ -25,7 +23,7 @@ class TotallyOrdered (α : Type _) extends LE α, LT α where
   le_trans : ∀ {a b c : α}, a ≤ b → b ≤ c → a ≤ c
   le_total : ∀ {a b : α}, a ≤ b ∨ b ≤ a
 
-class Fractional (α : Type _) extends CommMonoid α, TotallyOrdered α where
+class Numbers (α : Type _) extends CommMonoid α, TotallyOrdered α where
   lt_def : ∀ a b, a < b ↔ ∃ c : α, c > 0 ∧ a + c = b
   le_def : ∀ a b : α, a ≤ b ↔ a < b ∨ a = b
   lt_not_eq : ∀ {a : α}, a > 0 ↔ a ≠ 0
@@ -37,7 +35,7 @@ class Fractional (α : Type _) extends CommMonoid α, TotallyOrdered α where
   add_le_mono  : ∀ {a b c : α}, a + b ≤ c → a ≤ c
   lt_sum : ∀ {a b : α}, a < b ↔ ∃ r, a + r = b
 
-theorem lt_order_compat [iFrac : Fractional α] :
+theorem lt_order_compat [iFrac : Numbers α] :
   ∀ {a b c : α}, a < b → a + c < b + c := by
   intro a b c
   intro hab
@@ -48,16 +46,16 @@ theorem lt_order_compat [iFrac : Fractional α] :
   · exact hd_pos
   · conv =>
       lhs
-      rw [iFrac.assoc]
+      rw [iFrac.add_assoc]
       arg 2
-      rw [iFrac.comm]
+      rw [iFrac.add_comm]
     conv =>
       lhs
-      rw[←iFrac.assoc]
+      rw[←iFrac.add_assoc]
       rw[hab]
 
 
-theorem lt_alt_def [iFrac : Fractional α] :
+theorem lt_alt_def [iFrac : Numbers α] :
   ∀ {a b : α}, a < b ↔ a ≤ b ∧ a ≠ b := by
   intro a b
   constructor
@@ -79,7 +77,7 @@ theorem lt_alt_def [iFrac : Fractional α] :
 
 
 
-theorem le_alt_def [iFrac : Fractional α] :
+theorem le_alt_def [iFrac : Numbers α] :
   ∀ {a b : α}, a ≤ b ↔ ∃ c : α, a + c = b := by
   intro a b
   constructor
@@ -111,7 +109,7 @@ theorem le_alt_def [iFrac : Fractional α] :
     rw [this, iFrac.id_law] at hc
     exact hc
 
-theorem lt_trans [iFrac : Fractional α] :
+theorem lt_trans [iFrac : Numbers α] :
   ∀ {a b c : α}, a < b → b < c → a < c := by
   intro a b c hab hbc
   rw [iFrac.lt_def] at *
@@ -120,21 +118,21 @@ theorem lt_trans [iFrac : Fractional α] :
   exists (ad + bd)
   sorry
 
-theorem right_add_order_compat [iFrac : Fractional α] :
+theorem right_add_order_compat [iFrac : Numbers α] :
   ∀ a b c : α, a ≤ b → c + a ≤ c + b := by
   intro a b c h
-  conv => lhs; rw[iFrac.comm]
-  conv => rhs; rw[iFrac.comm]
+  conv => lhs; rw[iFrac.add_comm]
+  conv => rhs; rw[iFrac.add_comm]
   apply iFrac.add_order_compat h
 
 
-theorem right_cancel [iFrac : Fractional α] : ∀ a b c : α, a + c = b + c → a = b := by
+theorem right_cancel [iFrac : Numbers α] : ∀ a b c : α, a + c = b + c → a = b := by
   intro a b c h
-  conv at h => lhs; rw [iFrac.comm]
-  conv at h => rhs; rw [iFrac.comm]
+  conv at h => lhs; rw [iFrac.add_comm]
+  conv at h => rhs; rw [iFrac.add_comm]
   apply iFrac.left_cancel h
 
-theorem add_to_zero_is_zero [iFrac : Fractional α] :
+theorem add_to_zero_is_zero [iFrac : Numbers α] :
   ∀ {a b : α}, a + b = 0 → a = 0 ∧ b = 0 := by
   intro a b h
   constructor
@@ -143,25 +141,25 @@ theorem add_to_zero_is_zero [iFrac : Fractional α] :
       exists b
     have h₃ : a ≤ 0 := h' h_int
     exact iFrac.le_antisymm ⟨h₃, iFrac.positive⟩
-  · rw [iFrac.comm] at h
+  · rw [iFrac.add_comm] at h
     have h' := (@le_alt_def α iFrac b 0).mpr
     have h_int : ∃ c, b + c = 0 := by
       exists a
     have h₃ : b ≤ 0 := h' h_int
     exact iFrac.le_antisymm ⟨h₃, iFrac.positive⟩
 
-theorem positive [iFrac : Fractional α] : ∀ {a : α}, ¬∃ (b : α), a + b < a := by
+theorem positive [iFrac : Numbers α] : ∀ {a : α}, ¬∃ (b : α), a + b < a := by
   intro a ⟨b,hb⟩
   rw [iFrac.lt_def] at hb
   have ⟨c, hc_pos, hc_sum⟩ := hb
-  rw [iFrac.assoc] at hc_sum
+  rw [iFrac.add_assoc] at hc_sum
   conv at hc_sum => rhs; rw [←iFrac.id_law a]
   replace hc_sum := iFrac.left_cancel hc_sum
   have h' := add_to_zero_is_zero hc_sum
   simp at hc_pos
   exact iFrac.lt_not_eq.mp hc_pos h'.right
 
-theorem add_le_mono [iFrac : Fractional α] : ∀ {a b c : α}, a + b ≤ c → a ≤ c := by
+theorem add_le_mono [iFrac : Numbers α] : ∀ {a b c : α}, a + b ≤ c → a ≤ c := by
   intro a b c h
   rw[iFrac.le_def, iFrac.lt_def] at *
   obtain (⟨d,hd⟩ | ⟨d,hd⟩) := h
@@ -180,70 +178,76 @@ theorem add_le_mono [iFrac : Fractional α] : ∀ {a b c : α}, a + b ≤ c → 
 
 namespace Iris
 
-abbrev Frac (T : Type _) [Fractional T] := LeibnizO T
+abbrev Numerical (α : Type _) [Numbers α] := LeibnizO α
 
 -- TODO: How do I get rid of these instances?
 -- I definitely do not want to use an abbrev for Frac (there are many different OFE's for numerical types)
 -- so do we need to keep around all of these coercions?
-instance [Fractional α] : Coe (Frac α) α := ⟨(·.car)⟩
-instance [Fractional T] : Coe T (Frac T) := ⟨(⟨·⟩)⟩
-@[simp] instance [Fractional T] : COFE (Frac T) := by unfold Frac; infer_instance
-@[simp] instance [Fractional T] : One (Frac T) := ⟨⟨One.one⟩⟩
-@[simp] instance [Fractional T] : LE (Frac T) := ⟨fun x y => x.1 ≤ y.1⟩
-@[simp] instance [Fractional T] : LT (Frac T) := ⟨fun x y => x.1 < y.1⟩
-@[simp] instance [Fractional T] : Add (Frac T) := ⟨fun x y => x.1 + y.1⟩
+instance [Numbers α] : Coe (Numerical α) α := ⟨(·.car)⟩
+instance [Numbers T] : Coe T (Numerical T) := ⟨(⟨·⟩)⟩
+@[simp] instance [Numbers T] : COFE (Numerical T) := by unfold Numerical; infer_instance
+@[simp] instance [Numbers α] : Zero (Numerical α) := ⟨⟨Zero.zero⟩⟩
+@[simp] instance [Numbers T] : One (Numerical T) := ⟨⟨One.one⟩⟩
+@[simp] instance [Numbers T] : LE (Numerical T) := ⟨fun x y => x.1 ≤ y.1⟩
+@[simp] instance [Numbers T] : LT (Numerical T) := ⟨fun x y => x.1 < y.1⟩
+@[simp] instance [Numbers T] : Add (Numerical T) := ⟨fun x y => x.1 + y.1⟩
 
 namespace Frac
 
-variable [iF : Fractional α]
+variable [iF : Numbers α]
 
-instance Frac_CMRA : CMRA (Frac α) where
-  pcore _ := none
+instance Frac_CMRA : CMRA (Numerical α) where
+  pcore _ := some 0
   op := Add.add
-  ValidN _ x := x ≤ one
-  Valid x := x ≤ one
-  op_ne {x} := ⟨fun _ _ _ => congrArg (Add.add x)⟩
+  ValidN _ x := True
+  Valid x := True
+  op_ne {x} := ⟨fun n a b => by apply congrArg (Add.add x)⟩
   pcore_ne := fun _ => (exists_eq_right'.mpr ·)
-  validN_ne := (le_of_eq_of_le ∘ OFE.Dist.symm <| ·)
+  validN_ne := fun _ _ => True.intro
   valid_iff_validN := Iff.symm (forall_const Nat)
   validN_succ := (·)
-  validN_op_left := Fractional.add_le_mono
-  assoc := by simp [← iF.assoc]
-  comm := by simp [← iF.comm]
-  pcore_op_left := by simp
+  validN_op_left := by simp only [imp_self, implies_true]
+  assoc := by simp [← iF.add_assoc]
+  comm := by simp [← iF.add_comm]
+  pcore_op_left := by
+    intro ⟨x⟩ ⟨y⟩ hxy
+    cases hxy 
+    simp
+    rw[iF.add_comm]
+    simp[iF.id_law x]
   pcore_idem := by simp
   pcore_op_mono := by simp
   extend {_ _ y1 y2} _ _ := by exists y1; exists y2
 
 -- TODO: Simplify
-theorem frac_included {p q : Frac α} : p ≼ q ↔ p < q :=
+theorem num_included {p q : Numerical α} : p ≼ q ↔ p < q :=
   ⟨ by
       rintro ⟨r, Hr⟩
-      apply Fractional.lt_sum.mpr
+      apply Numbers.lt_sum.mpr
       exists r
       rw [Hr]
       rfl,
     by
       intro H
-      rcases Fractional.lt_sum.mp H with ⟨r, Hr⟩
+      rcases Numbers.lt_sum.mp H with ⟨r, Hr⟩
       exists r
       simp [Iris.Frac.Frac_CMRA]
       rw [Hr]
       rfl⟩
 
-theorem frac_included_weak {p q : Frac α} (H : p ≼ q) : p ≤ q := by
-  have h := iF.lt_def_le.mp (frac_included.mp H)
+theorem frac_included_weak {p q : Numerical α} (H : p ≼ q) : p ≤ q := by
+  have h := iF.lt_def_le.mp (num_included.mp H)
   simp [h]
 
-instance : CMRA.Discrete (Frac α) where
+instance : CMRA.Discrete (Numerical α) where
   discrete_0 := id
   discrete_valid := id
 
-instance : CMRA.Exclusive (one : Frac α) where
-  exclusive0_l _ H := Fractional.positive ⟨_, H⟩
+instance : CMRA.Exclusive (one : Numerical α) where
+  exclusive0_l _ H := Numerical.positive ⟨_, H⟩
 
 -- TODO: Simplify
-instance {q : Frac α} : CMRA.Cancelable q where
+instance {q : Numerical α} : CMRA.Cancelable q where
   cancelableN {n x y} := by
     simp [CMRA.ValidN]
     intro _
@@ -257,15 +261,15 @@ instance {q : Frac α} : CMRA.Cancelable q where
     simp_all [Add.add]
     rw [H']
     simp [HAdd.hAdd] at H
-    have H'' : ({ car := Add.add q x } : Frac α).car = ({ car := Add.add q y } : Frac α).car := by
+    have H'' : ({ car := Add.add q x } : Numerical α).car = ({ car := Add.add q y } : Numerical α).car := by
       rw [H]
     exact H''
 
 -- TODO: Simplify
-instance {q : Frac α} : CMRA.IdFree q where
+instance {q : Numerical α} : CMRA.IdFree q where
   id_free0_r y := by
     intro H H'
-    apply @Fractional.positive (α) (a := q)
+    apply @Numbers.positive (α) (a := q)
     exists y
     conv=>
       rhs
