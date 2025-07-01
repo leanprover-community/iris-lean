24e39e1e87e70a62ecb9ca6544d44b2334241a99
Shreyas <shreyasss94@gmail.com>
2025-07-01 13:35:18 +0200

    Fix copyright header

--- src/Iris/Algebra/Numbers.lean
+++ src/Iris/Algebra/Numbers.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Shreyas Srinivas. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shreyas Srinivas, Markus de Medeiros
+-/
+
 import Iris.Algebra.CMRA
 import Iris.Algebra.OFE
 
