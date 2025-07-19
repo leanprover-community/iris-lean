46ea9d1eca075c9e600b8143f9561109ed33e85c
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-07-19 14:03:05 -0400

    fix build

--- src/Iris/Instances/heProp.lean
+++ src/Iris/Instances/heProp.lean
@@ -29,17 +29,19 @@ open Iris
 section heProp
 
 variable (F K V : Type _) (H : Type _ → Type _) [UFraction F] [∀ T, Heap (H T) K T]
+variable [∀ V, HasHHMap (H V) (H (Agree (LeibnizO V))) K V (Agree (LeibnizO V))]
+variable [∀ V, HasHHMap (H V) (H (DFrac F × Agree (LeibnizO V))) K V (DFrac F × Agree (LeibnizO V))]
 
 abbrev heProp := UPred (HeapView F K (Agree (LeibnizO V)) H)
 
 def heProp_auth (m : H V) : heProp F K V H :=
-  UPred.ownM <| ●V StoreO.map (.lift <| toAgree ∘ .mk) ⟨m⟩
+  UPred.ownM <| ●V HasHHMap.hhmap (fun (_ : K) (v : V) => some (toAgree <| .mk v)) m
 
 def heProp_frag (m : H V) : heProp F K V H :=
-  UPred.ownM <| ◯V StoreO.map (.lift fun v => (.own One.one, toAgree <| .mk v)) ⟨m⟩
+  UPred.ownM <| ◯V HasHHMap.hhmap (fun (_ : K) (v : V) => some (DFrac.own 1, toAgree <| .mk v)) m
 
 def heProp_elem (k : K) (v : V) : heProp F K V H :=
-  heProp_frag F K V H (WithPoints.point k (.some v))
+  heProp_frag F K V H (Heap.point k (.some v))
 
 notation k " ↦ " v => heProp_elem k v
 
