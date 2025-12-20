5be19043639747f6b8ca815add790c5f8e7d2938
Alex Bai <ahuoguo@gmail.com>
2025-12-20 12:09:04 +0800

    Feat: BUpdPlain
    Port of BupdAlt, a generic definition for basic updates (BUpd), which works for any BIPlainly BI
    
    ---------
    
    Co-authored-by: Markus de Medeiros <markusdemedeiros@outlook.com>

--- src/Iris/Examples/Resources.lean
+++ src/Iris/Examples/Resources.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Markus de Medeiros. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus de Medeiros
 -/
-import Iris.BI
 import Iris.ProofMode
 import Iris.Algebra.IProp
 import Iris.Instances.UPred.Instance
