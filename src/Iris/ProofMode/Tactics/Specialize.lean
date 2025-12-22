9a8a1b781a5233fabbd39a45405af6ba6761ec6d
Michael Sammler <noreply@sammler.me>
2025-12-22 15:28:10 +0100

    Add ProofModeM monad
    Add a ProofModeM monad that automates the goal management of proof
    mode tactics. This subsumes the previous `Goals` structure.

--- src/Iris/ProofMode/Tactics/Specialize.lean
+++ src/Iris/ProofMode/Tactics/Specialize.lean
@@ -38,8 +38,8 @@ theorem specialize_forall [BI PROP] {p : Bool} {A1 A2 P : PROP} {α : Sort _} {�
     [inst : IntoForall P Φ] (h : A1 ⊢ A2 ∗ □?p P) (a : α) : A1 ⊢ A2 ∗ □?p (Φ a) := by
   refine h.trans <| sep_mono_r <| intuitionisticallyIf_mono <| inst.1.trans (forall_elim a)
 
-def SpecializeState.process_wand (gs : @Goals u prop bi) :
-    @SpecializeState u prop bi orig → SpecPat → TermElabM (SpecializeState bi orig)
+def SpecializeState.process_wand :
+    @SpecializeState u prop bi orig → SpecPat → ProofModeM (SpecializeState bi orig)
   | { hyps, p, out, pf, .. }, .ident i => do
     let uniq ← hyps.findWithInfo i
     let ⟨e', hyps', out₁, out₁', p1, _, pf'⟩ := hyps.remove false uniq
@@ -48,7 +48,7 @@ def SpecializeState.process_wand (gs : @Goals u prop bi) :
     have : $p2 =Q ($p1 && $p) := ⟨⟩
 
     let out₂ ← mkFreshExprMVarQ prop
-    let some _ ← ProofMode.trySynthInstanceQAddingGoals gs q(IntoWand $p $p1 $out .in $out₁' .out $out₂) |
+    let some _ ← ProofModeM.trySynthInstanceQ q(IntoWand $p $p1 $out .in $out₁' .out $out₂) |
       throwError m!"ispecialize: cannot instantiate {out} with {out₁'}"
     let pf := q(specialize_wand $pf $pf')
     return { e := e', hyps := hyps', p := p2, out := out₂, pf }
@@ -56,12 +56,12 @@ def SpecializeState.process_wand (gs : @Goals u prop bi) :
     let v ← mkFreshLevelMVar
     let α : Q(Sort v) ← mkFreshExprMVarQ q(Sort v)
     let Φ : Q($α → $prop) ← mkFreshExprMVarQ q($α → $prop)
-    let some _ ← ProofMode.trySynthInstanceQAddingGoals gs q(IntoForall $out $Φ) | throwError "ispecialize: {out} is not a lean premise"
+    let some _ ← ProofModeM.trySynthInstanceQ q(IntoForall $out $Φ) | throwError "ispecialize: {out} is not a lean premise"
     let x ← elabTermEnsuringTypeQ (u := .succ .zero) t α
     have out' : Q($prop) := Expr.headBeta q($Φ $x)
     have : $out' =Q $Φ $x := ⟨⟩
     let newMVarIds ← getMVarsNoDelayed x
-    for mvar in newMVarIds do gs.addPureGoal mvar
+    for mvar in newMVarIds do addMVarGoal mvar
     return { e, hyps, p, out := out', pf := q(specialize_forall $pf $x) }
   | { hyps, p, out, pf, .. }, .goal ns g => do
     let mut uniqs : NameSet := {}
@@ -70,31 +70,28 @@ def SpecializeState.process_wand (gs : @Goals u prop bi) :
     let ⟨el', _, hypsl', hypsr', h'⟩ := Hyps.split bi (λ _ uniq => uniqs.contains uniq) hyps
     let out₁ ← mkFreshExprMVarQ prop
     let out₂ ← mkFreshExprMVarQ prop
-    let some _ ← ProofMode.trySynthInstanceQAddingGoals gs q(IntoWand $p false $out .out $out₁ .out $out₂) | throwError m!"ispecialize: {out} is not a wand"
-    let pf' ← gs.addGoal hypsr' out₁ g
+    let some _ ← ProofModeM.trySynthInstanceQ q(IntoWand $p false $out .out $out₁ .out $out₂) | throwError m!"ispecialize: {out} is not a wand"
+    let pf' ← addBIGoal hypsr' out₁ g
     let pf := q(specialize_wand_subgoal $out₂ $pf $h' $pf')
     return { e := el', hyps := hypsl', p := q(false), out := out₂, pf }
 
-def iSpecializeCore (gs : @Goals u prop bi) {e} (hyps : Hyps bi e)
-  (uniq : Name) (spats : List SpecPat) : TacticM ((e' : _) × Hyps bi e' × Q($e ⊢ $e')) := do
+def iSpecializeCore {e} (hyps : @Hyps u prop bi e)
+  (uniq : Name) (spats : List SpecPat) : ProofModeM ((e' : _) × Hyps bi e' × Q($e ⊢ $e')) := do
   let some ⟨name, _, hyps, _, out, p, _, pf⟩ := Id.run <|
     hyps.removeG true λ name uniq' _ _ => if uniq == uniq' then some name else none
     | throwError "ispecialize: cannot find argument"
   let state := { hyps, out, p, pf := q(($pf).1), .. }
-  let state ← liftM <| spats.foldlM (SpecializeState.process_wand gs) state
+  let state ← spats.foldlM (SpecializeState.process_wand) state
 
   let hyps' := Hyps.add bi name uniq state.p state.out state.hyps
   return ⟨_, hyps', state.pf⟩
 
 elab "ispecialize" colGt pmt:pmTerm : tactic => do
   let pmt ← liftMacroM <| PMTerm.parse pmt
-  let (mvar, { bi, hyps, goal, .. }) ← istart (← getMainGoal)
-  mvar.withContext do
-  let gs ← Goals.new bi
+  ProofModeM.runTactic λ mvar { hyps, goal, .. } => do
   -- hypothesis must be in the context, otherwise use pose proof
   let name := ⟨pmt.term⟩
   let some uniq ← try? <| hyps.findWithInfo name | throwError s!"{name} should be a hypothesis, use ihave instead"
-  let ⟨_, hyps', pf⟩ ← iSpecializeCore gs hyps uniq pmt.spats
-  let pf' ← gs.addGoal hyps' goal
+  let ⟨_, hyps', pf⟩ ← iSpecializeCore hyps uniq pmt.spats
+  let pf' ← addBIGoal hyps' goal
   mvar.assign q(($pf).trans $pf')
-  replaceMainGoal (← gs.getGoals)
