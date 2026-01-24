d398e8d32bdfe76c189282a38d7eb6714e3ace9b
ScreenL <915402966@qq.com>
2026-01-24 22:02:41 +0800

    names nit

--- src/Iris/Algebra/Agree.lean
+++ src/Iris/Algebra/Agree.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Leo Stefanesco. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leo Stefanesco
+Authors: Leo Stefanesco, Puming Liu
 -/
 
 import Iris.Algebra.CMRA
