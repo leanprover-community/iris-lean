d035645a398a3393db017bfbd9d5364a13ed6388
Markus de Medeiros <markusdemedeiros@outlook.com>
2026-02-03 14:11:02 -0500

    change Heap API to use PartialMap

--- src/Iris/Std/Heap.lean
+++ src/Iris/Std/Heap.lean
@@ -10,59 +10,60 @@ import Iris.Std.PartialMap
 
 section Store
 
-/-- The type `T` can store and retrieve values of type `V` using keys of type `K`. -/
-class Store (T : Type _) (K V : outParam (Type _)) where
-  get : T → K → V
-  set : T → K → V → T
-  get_set_eq {t k k' v} : k = k' → get (set t k v) k' = v
-  get_set_ne {t k k' v} : k ≠ k' → get (set t k v) k' = get t k'
-export Store (get set get_set_eq get_set_ne)
+open Iris Std PartialMap
 
-open Classical in
-theorem Store.get_set [Store T K V] {t : T} {k k' : K} {v : V} :
-    get (set t k v) k' = if k = k' then v else get t k' := by
-  by_cases h : k = k'
-  · rw [if_pos h, get_set_eq h]
-  · rw [if_neg h, get_set_ne h]
 
-/-- An index-dependent predicate holds at every member of the store. -/
-def Store.all [Store T K V] (P : K → V → Prop) : T → Prop :=
-  fun t => ∀ k, P k (get t k)
+-- /-- The type `T` can store and retrieve values of type `V` using keys of type `K`. -/
+-- class Store (T : Type _) (K V : outParam (Type _)) where
+--   get : T → K → V
+--   set : T → K → V → T
+--   get_set_eq {t k k' v} : k = k' → get (set t k v) k' = v
+--   get_set_ne {t k k' v} : k ≠ k' → get (set t k v) k' = get t k'
+-- export Store (get set get_set_eq get_set_ne)
+--
+-- open Classical in
+-- theorem Store.get_set [Store T K V] {t : T} {k k' : K} {v : V} :
+--     get (set t k v) k' = if k = k' then v else get t k' := by
+--   by_cases h : k = k'
+--   · rw [if_pos h, get_set_eq h]
+--   · rw [if_neg h, get_set_ne h]
 
-/-- RepFunStore: The type T can represent all functions that satisfy Rep.
-For instance, this holds with `rep _ = True` when `T = K → V`. On the other hand, when
-`T = list (K × Option V)` representing a partial function with an association list, this holds
-when `rep f` is the predicate that says `f` has finite domain. -/
-class RepFunStore (T : Type _) (K V : outParam (Type _)) [Store T K V] where
-  rep : (K → V) → Prop
-  rep_get (t : T) : rep (get t)
-  of_fun : { f : K → V // rep f } → T
-  get_of_fun : get (of_fun f) = f
-export RepFunStore (rep rep_get of_fun get_of_fun)
+-- /-- An index-dependent predicate holds at every member of the store. -/
+-- def Store.all [Store T K V] (P : K → V → Prop) : T → Prop :=
+--   fun t => ∀ k, P k (get t k)
 
-/-- IsoFunStore: The type T is isomorphic the type of functions `{f : Rep f}`, or in other words,
-equality of T is the same as equality of `{f : Rep f}`. -/
-class IsoFunStore (T : Type _) (K V : outParam (Type _)) [Store T K V] extends RepFunStore T K V where
-  of_fun_get {t : T} : of_fun ⟨get t, rep_get t⟩ = t
-export IsoFunStore (of_fun_get)
 
-theorem IsoFunStore.ext [Store T K V] [IsoFunStore T K V] {t1 t2 : T}
-    (H : ∀ k, get t1 k = get t2 k) : t1 = t2 := by
+/-- RepFunMap: The map T is capable of representing all partial functions out of K. -/
+class RepFunMap (T : Type _ → Type _) (K : outParam (Type _)) [PartialMap T K] where
+  of_fun : (K → Option V) → T V
+  get_of_fun (f : K → Option V) (k : K) : get? (of_fun f) k = f k
+export RepFunMap (of_fun get_of_fun)
+
+/-- IsoFunStore: The map T is isomorphic to the type of functions out of `K`. In other words,
+    equality of T is the same as equality of functions, so the CMRA on these partial functions
+    is leibniz. -/
+class IsoFunMap (T : Type _  → Type _) (K : outParam (Type _)) [PartialMap T K]
+  extends RepFunMap T K where
+  of_fun_get {t : T V} : of_fun (get? t) = t
+export IsoFunMap (of_fun_get)
+
+theorem IsoFunStore.ext [PartialMap T K] [IsoFunMap T K] {t1 t2 : T V}
+    (H : ∀ k, get? t1 k = get? t2 k) : t1 = t2 := by
   rw [← of_fun_get (t := t1), ← of_fun_get (t := t2)]
   simp only [funext H]
 
 -- theorem IsoFunStore.store_eq_of_Equiv [Store T K V] [IsoFunStore T K V] {t1 t2 : T}
 --     (H : Store.Equiv t1 t2) : t1 = t2 := IsoFunStore.ext <| congrFun H
 
-/-- Stores of type T1 can be coerced to stores of type T2. -/
-class HasStoreMap (T1 T2 : Type _) (K V1 V2 : outParam (Type _)) [Store T1 K V1] [Store T2 K V2] where
-  /-- Map a function that depends on the element across the entire structure  -/
-  dmap (f : K → V1 → V2) : T1 → T2
-  get_dmap : get (dmap f t) k = f k (get t k)
-export HasStoreMap (dmap get_dmap)
-
-def HasStoreMap.map (f : V1 → V2) [Store T1 K V1] [Store T2 K V2] [HasStoreMap T1 T2 K V1 V2] : T1 → T2 :=
-  HasStoreMap.dmap (fun _ => f)
+-- /-- Stores of type T1 can be coerced to stores of type T2. -/
+-- class HasStoreMap (T1 T2 : Type _) (K V1 V2 : outParam (Type _)) [Store T1 K V1] [Store T2 K V2] where
+--   /-- Map a function that depends on the element across the entire structure  -/
+--   dmap (f : K → V1 → V2) : T1 → T2
+--   get_dmap : get (dmap f t) k = f k (get t k)
+-- export HasStoreMap (dmap get_dmap)
+--
+-- def HasStoreMap.map (f : V1 → V2) [Store T1 K V1] [Store T2 K V2] [HasStoreMap T1 T2 K V1 V2] : T1 → T2 :=
+--   HasStoreMap.dmap (fun _ => f)
 
 end Store
 
@@ -70,70 +71,45 @@ section Heap
 
 open Iris.Std
 
-/-- Map between heaps that preserves non-allocations. -/
-class HasHeapMap (T1 T2 : Type _) (K V1 V2 : outParam (Type _))
-   [Store T1 K (Option V1)] [Store T2 K (Option V2)] where
-  hhmap (f : K → V1 → Option V2) : T1 → T2
-  hhmap_get (f : K → V1 → Option V2) : get (hhmap f t) k = (get t k).bind (f k)
-export HasHeapMap (hhmap hhmap_get)
+-- /-- Map between heaps that preserves non-allocations. -/
+-- class HasHeapMap (T1 T2 : Type _) (K V1 V2 : outParam (Type _))
+--    [Store T1 K (Option V1)] [Store T2 K (Option V2)] where
+--   hhmap (f : K → V1 → Option V2) : T1 → T2
+--   hhmap_get (f : K → V1 → Option V2) : get (hhmap f t) k = (get t k).bind (f k)
+-- export HasHeapMap (hhmap hhmap_get)
 
 /-- Heap extends PartialMap with heap-specific operations for separation logic.
     The heap stores optional values, where `none` represents unallocated locations. -/
-class Heap (K : outParam (Type _)) (M : Type _ → Type _) extends PartialMap M K where
-  /-- Map operation that can optionally remove entries. -/
-  hmap (f : K → V → Option V) : M V → M V
+class Heap (M : Type _ → Type _) (K : outParam (Type _))
+    extends PartialMap M K where
   /-- Merge two heaps with a combining operation. -/
-  merge (op : V → V → V) : M V → M V → M V
-  /-- get? on hmap applies the function to present values. -/
-  get?_hmap : get? (hmap f m) k = (get? m k).bind (f k)
+  merge (op : V1 → V2 → V) : M V1 → M V2 → M V
   /-- get? on merge combines values using Option.merge. -/
   get?_merge : get? (merge op m₁ m₂) k = Option.merge op (get? m₁ k) (get? m₂ k)
+export Heap (merge get?_merge)
 
-export Heap (hmap merge get?_hmap get?_merge)
-
-theorem hmap_alloc [Heap K M] {m : M V} (H : get? m k = some v) : get? (hmap f m) k = f k v := by
-  simp [get?_hmap, H]
-
-theorem hmap_unalloc [Heap K M] {m : M V} (H : get? m k = none) : get? (hmap f m) k = none := by
-  simp [get?_hmap, H]
-
-/-- The heap of a single point (singleton with optional value). -/
-def Heap.point [Heap K M] (k : K) (v : Option V) : M V :=
-  match v with
-  | some v' => PartialMap.insert empty k v'
-  | none => empty
+-- Changed to: singleton
+-- /-- The heap of a single point (singleton with optional value). -/
+-- def Heap.point [Heap K M] (k : K) (v : Option V) : M V :=
+--   match v with
+--   | some v' => PartialMap.insert empty k v'
+--   | none => empty
 
 /-- The domain of a heap is the set of keys that map to .some values. -/
-def Heap.dom [Heap K M] (m : M V) : K → Prop := fun k => (get? m k).isSome
+def Heap.dom [Heap M K] (m : M V) : K → Prop := fun k => (get? m k).isSome
 
-@[simp] def Heap.union [Heap K M] : M V → M V → M V := merge (fun v _ => v)
-
-theorem Heap.point_get?_eq [Heap K M] [DecidableEq K] [LawfulPartialMap M K] (H : k = k') : get? (point k (some v) : M V) k' = some v :=
-  get?_insert_eq  H
-
-
-theorem Heap.point_get?_ne [Heap K M] [DecidableEq K] [LawfulPartialMap M K] (H : k ≠ k') :
-    get? (point k (some v) : M V) k' = none := by
-  rw [point]
-  rw [get?_insert_ne H]
-  exact get?_empty k'
-
-open Classical in
-theorem Heap.get?_point [Heap K M] [DecidableEq K] [LawfulPartialMap M K] {k k' : K} {v : V} :
-    get? (point k (some v) : M V) k' = if k = k' then some v else none := by
-  by_cases h : k = k'
-  · rw [if_pos h, h]; exact Heap.point_get?_eq rfl
-  · rw [if_neg h]; exact Heap.point_get?_ne h
+-- Should this be part of the typeclass, or should we use this derived one?
+@[simp] def Heap.union [Heap M K] : M V → M V → M V := merge (fun v _ => v)
 
 /-- An AllocHeap is a heap which can allocate elements under some condition. -/
-class AllocHeap (K : outParam (Type _)) (M : Type _ → Type _) extends Heap K M where
+class AllocHeap (M : Type _ → Type _) (K : outParam (Type _)) extends Heap M K where
   notFull : M V → Prop
   fresh {m : M V} : notFull m → K
   get?_fresh {m : M V} {H : notFull m} : get? m (fresh H) = none
 export AllocHeap (notFull fresh get?_fresh)
 
 /-- An UnboundedHeap is a heap which can allocate an unbounded number of elements starting at empty. -/
-class UnboundedHeap (K : outParam (Type _)) (M : Type _ → Type _) extends AllocHeap K M where
+class UnboundedHeap (M : Type _ → Type _) (K : outParam (Type _)) extends AllocHeap M K where
   notFull_empty : notFull (empty : M V)
   notFull_insert_fresh {m : M V} {H : notFull m} : notFull (insert m (fresh H) v)
 export UnboundedHeap (notFull_empty notFull_insert_fresh)
--- src/Iris/Std/PartialMap.lean
+++ src/Iris/Std/PartialMap.lean
@@ -90,6 +90,20 @@ class LawfulPartialMap (M : Type _ → Type _) (K : outParam (Type _)) [PartialM
   get?_delete_ne {m : M V} {k k'} : k ≠ k' → get? (delete m k) k' = get? m k'
 export LawfulPartialMap (get?_empty get?_insert_eq get?_insert_ne get?_delete_eq get?_delete_ne)
 
+/-- A functorial partial map is a partial map, whose functor map function preserves the non-allocation
+    of elements. -/
+class FunctorialPartialMap (M : Type _ → Type _) (K : outParam (Type _))
+    extends PartialMap M K, Functor M where
+  get?_map {m : M V} {f : K → V → Option V'} {k} :
+    get? (f k <$> m) k = (get? m k).bind (f k)
+export FunctorialPartialMap (get?_map)
+
+theorem map_some [FunctorialPartialMap M K] {m : M V} {f : K → V → Option V'} (H : get? m k = some v) :
+    get? (f k <$> m) k = f k v := by simp [get?_map, H]
+
+theorem map_none [FunctorialPartialMap M K] {m : M V} {f : K → V → Option V'} (H : get? m k = none) :
+    get? (f k <$> m) k = none := by simp [get?_map, H]
+
 namespace PartialMap
 
 variable {K : Type v} {M : Type u → _} [PartialMap M K]
