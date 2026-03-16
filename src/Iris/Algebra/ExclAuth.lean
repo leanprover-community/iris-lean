a82f079cbbca0b0076e6c93c8c1ca47790a05efb
Markus de Medeiros <markusdemedeiros@outlook.com>
2026-03-16 13:52:15 -0400

    chore: cleanup

--- src/Iris/Algebra/ExclAuth.lean
+++ src/Iris/Algebra/ExclAuth.lean
@@ -47,64 +47,54 @@ instance frag_ne : NonExpansive (frag (F := F) (A := A)) where
   ne _ _ _ h := Auth.frag_ne.ne (some_dist_some.mpr h)
 
 @[rocq_alias excl_auth_auth_discrete]
-instance auth_discrete {a : A} [DiscreteE a] :
-    DiscreteE (●E a : ExclAuthR (F := F) (A := A)) :=
+instance auth_discrete {a : A} [DiscreteE a] : DiscreteE (●E a : ExclAuthR (F := F)) :=
   Auth.auth_discrete (some_is_discrete inferInstance) none_is_discrete
 
 @[rocq_alias excl_auth_frag_discrete]
-instance frag_discrete {a : A} [DiscreteE a] :
-    DiscreteE (◯E a : ExclAuthR (F := F) (A := A)) :=
+instance frag_discrete {a : A} [DiscreteE a] : DiscreteE (◯E a : ExclAuthR (F := F)) :=
   Auth.frag_discrete (some_is_discrete inferInstance)
 
 @[rocq_alias excl_auth_validN]
-theorem validN {n : Nat} {a : A} : ✓{n} ((●E a : ExclAuthR (F := F) (A := A)) • ◯E a) :=
+theorem validN {n : Nat} {a : A} : ✓{n} (●E a : ExclAuthR (F := F)) • ◯E a :=
   Auth.both_validN.mpr ⟨.rfl, trivial⟩
 
 @[rocq_alias excl_auth_valid]
-theorem valid {a : A} : ✓ ((●E a : ExclAuthR (F := F) (A := A)) • ◯E a) :=
+theorem valid {a : A} : ✓ (●E a : ExclAuthR (F := F)) • ◯E a :=
   Auth.auth_both_valid_2 trivial .rfl
 
 @[rocq_alias excl_auth_agreeN]
-theorem agreeN {n : Nat} {a b : A} (h : ✓{n} ((●E a : ExclAuthR (F := F) (A := A)) • ◯E b)) :
-    a ≡{n}≡ b :=
-  (dist_of_inc_exclusive (Auth.both_validN.mp h).1 trivial).symm
+theorem agreeN {n : Nat} {a b : A} (h : ✓{n} (●E a : ExclAuthR (F := F)) • ◯E b) :
+    a ≡{n}≡ b := dist_of_inc_exclusive (Auth.both_validN.mp h).1 trivial |>.symm
 
 @[rocq_alias excl_auth_agree]
-theorem agree {a b : A} (h : ✓ ((●E a : ExclAuthR (F := F) (A := A)) • ◯E b)) : a ≡ b :=
+theorem agree {a b : A} (h : ✓ (●E a : ExclAuthR (F := F)) • ◯E b) : a ≡ b :=
   equiv_dist.mpr fun _ => agreeN (Valid.validN h)
 
 @[rocq_alias excl_auth_agree_L]
-theorem agree_L [Leibniz A] {a b : A} (h : ✓ ((●E a : ExclAuthR (F := F) (A := A)) • ◯E b)) :
+theorem agree_L [Leibniz A] {a b : A} (h : ✓ (●E a : ExclAuthR (F := F)) • ◯E b) :
     a = b :=
   eq_of_eqv (agree h)
 
 @[rocq_alias excl_auth_auth_op_validN]
-theorem auth_op_validN {n : Nat} {a b : A} :
-    (✓{n} ((●E a : ExclAuthR (F := F) (A := A)) • ●E b)) ↔ False :=
+theorem auth_op_validN {n : Nat} {a b : A} : (✓{n} ((●E a : ExclAuthR (F := F)) • ●E b)) ↔ False :=
   Auth.auth_op_validN
 
 @[rocq_alias excl_auth_auth_op_valid]
-theorem auth_op_valid {a b : A} :
-    (✓ ((●E a : ExclAuthR (F := F) (A := A)) • ●E b)) ↔ False :=
+theorem auth_op_valid {a b : A} : (✓ (●E a : ExclAuthR (F := F)) • ●E b) ↔ False :=
   Auth.auth_op_valid
 
 @[rocq_alias excl_auth_frag_op_validN]
-theorem frag_op_validN {n : Nat} {a b : A} :
-    (✓{n} ((◯E a : ExclAuthR (F := F) (A := A)) • ◯E b)) ↔ False := by
-  rw [show ((◯E a : ExclAuthR (F := F) (A := A)) • ◯E b) =
-    (◯ (some (excl a) • some (excl b))) from Auth.frag_op.symm, Auth.frag_validN]
-  exact ⟨fun h => not_valid_some_exclN_op_left h, False.elim⟩
+theorem frag_op_validN  {a b : A} : (✓{n} (◯E a : ExclAuthR (F := F)) • ◯E b) ↔ False := by
+  suffices H : ✓{n} some (excl a) • some (excl b) ↔ False by rwa [Auth.frag_op.symm, Auth.frag_validN]
+  exact ⟨not_valid_some_exclN_op_left, False.elim⟩
 
 @[rocq_alias excl_auth_frag_op_valid]
-theorem frag_op_valid {a b : A} :
-    (✓ ((◯E a : ExclAuthR (F := F) (A := A)) • ◯E b)) ↔ False := by
-  rw [show ((◯E a : ExclAuthR (F := F) (A := A)) • ◯E b) =
-    (◯ (some (excl a) • some (excl b))) from Auth.frag_op.symm, Auth.frag_valid]
+theorem frag_op_valid {a b : A} : (✓ (◯E a : ExclAuthR (F := F)) • ◯E b) ↔ False := by
+  suffices H : ✓ some (excl a) • some (excl b) ↔ False by rwa [Auth.frag_op.symm, Auth.frag_valid]
   exact ⟨fun h => not_valid_some_exclN_op_left (n := 0) h.validN, False.elim⟩
 
 @[rocq_alias excl_auth_update]
-theorem update {a b a' : A} :
-    ((●E a : ExclAuthR (F := F) (A := A)) • ◯E b) ~~> ((●E a') • ◯E a') :=
+theorem update {a b a' : A} : ((●E a : ExclAuthR (F := F)) • ◯E b) ~~> ((●E a') • ◯E a') :=
   Auth.auth_update (.option (.exclusive (x' := excl a') trivial))
 
 /-! ## Functors -/
@@ -118,5 +108,4 @@ abbrev ExclAuthRF (T : COFE.OFunctorPre) [URFunctor T] : COFE.OFunctorPre :=
   AuthRF (F := F) (OptionOF (ExclOF T))
 
 end ExclAuth
-
 end Iris
