c77fdbb60a87e87b3dc9d17277aa024af16da4b8
Markus de Medeiros <markusdemedeiros@outlook.com>
2026-03-16 10:04:57 -0400

    chore: minor cleanup for Positives

--- src/Iris/Std/Positives.lean
+++ src/Iris/Std/Positives.lean
@@ -142,34 +142,24 @@ def reverse : Pos → Pos := reverseGo P1
 
 theorem reverse_go_app (p1 p2 p3 : Pos) :
   reverseGo p1 (p2 ++ p3) = reverseGo p1 p3 ++ reverseGo P1 p2 := by
-  have helper : ∀ p1 p2 p3, reverseGo (p2 ++ p3) p1 = p2 ++ (reverseGo p3 p1) := by
-    intro p1
+  have helper (p1 : Pos) : ∀ p2 p3, reverseGo (p2 ++ p3) p1 = p2 ++ (reverseGo p3 p1) := by
     induction p1 with
-    | xI p1 IH =>
-      intro p2 p3
-      apply (IH _ (_~1))
-    | xO p1 IH =>
-      intro p2 p3
-      apply (IH _ (_~0))
-    | xH =>
-      intro p2 p3; rfl
-
+    | xI _ ih => exact fun _ _ => ih _ (_~1)
+    | xO _ ih => exact fun _ _ => ih _ (_~0)
+    | xH => exact fun _ _ => rfl
   induction p3 generalizing p1 p2 with
-  | xI p3 IH =>
-    exact IH (p1~1) p2
-  | xO p3 IH =>
-    exact IH (p1~0) p2
-  | xH =>
-    rewrite [<- helper]
-    rfl
+  | xI p3 IH => exact IH (p1~1) p2
+  | xO p3 IH => exact IH (p1~0) p2
+  | xH => rw [<- helper]; rfl
 
-theorem reverse_app (p1 p2 : Pos) :
-  reverse (p1 ++ p2) = reverse p2 ++ reverse p1 := by
+theorem reverse_app (p1 p2 : Pos) : reverse (p1 ++ p2) = reverse p2 ++ reverse p1 := by
   simp [reverse]; apply reverse_go_app
-theorem reverse_x0 p : reverse (p~0) = (P1~0) ++ reverse p := by
-  apply (reverse_app p (P1~0))
-theorem reverse_xI p : reverse (p~1) = (P1~1) ++ reverse p := by
-  apply (reverse_app p (P1~1))
+
+theorem reverse_x0 p : reverse (p~0) = (P1~0) ++ reverse p :=
+  reverse_app p (P1~0)
+
+theorem reverse_xI p : reverse (p~1) = (P1~1) ++ reverse p :=
+  reverse_app p (P1~1)
 
 /-- Duplicate the bits of a positive, i.e. 1~0~1 -> 1~0~0~1~1 and
       1~1~0~0 -> 1~1~1~0~0~0~0 -/
@@ -199,11 +189,7 @@ and then separating each element with [10]. -/
 def flatten (xs : List Pos) : Pos :=
   flattenGo xs P1
 
-def unflattenGo
-        (p : Pos)
-        (acc_xs : List Pos)
-        (acc_elm : Pos)
-  : Option (List Pos) :=
+def unflattenGo (p : Pos) (acc_xs : List Pos) (acc_elm : Pos) : Option (List Pos) :=
   match p with
   | xH => some acc_xs
   | p'~0~0 => unflattenGo p' acc_xs (acc_elm~0)
@@ -216,29 +202,26 @@ used by [flatten]. -/
 def unflatten (p : Pos) : Option (List Pos) :=
   unflattenGo p [] P1
 
-theorem flatten_go_app xs acc :
-    flattenGo xs acc = acc ++ flattenGo xs P1 := by
+theorem flatten_go_app xs acc : flattenGo xs acc = acc ++ flattenGo xs P1 := by
   induction xs generalizing acc with
   | nil => rfl
   | cons x xs IH =>
-    simp [flattenGo]
-    rewrite [IH]
-    rewrite [IH (P1~1~0 ++ x.dup.reverse)]
-    simp only [<- app_assoc.assoc]
+    simp only [flattenGo]
+    rw [IH, IH (P1~1~0 ++ x.dup.reverse)]
+    simp [<- app_assoc.assoc]
     rfl
 
 theorem unflatten_go_app (p : Pos) suffix xs acc :
-  unflattenGo (suffix ++ reverse (dup p)) xs acc =
-  unflattenGo suffix xs (acc ++ p) := by
+  unflattenGo (suffix ++ reverse (dup p)) xs acc = unflattenGo suffix xs (acc ++ p) := by
   induction p generalizing suffix acc with
   | xI p IH =>
-    simp [dup]
-    rewrite [reverse_xI]; rewrite [reverse_xI]
+    simp only [dup]
+    rewrite [reverse_xI, reverse_xI]
     simp only [<- app_assoc.assoc]
     rewrite [IH]
     rfl
   | xO p IH =>
-    simp [dup]
+    simp only [dup]
     rewrite [reverse_x0]; rewrite [reverse_x0]
     simp only [<- app_assoc.assoc]
     rewrite [IH]
@@ -246,8 +229,7 @@ theorem unflatten_go_app (p : Pos) suffix xs acc :
   | xH => rfl
 
 theorem unflatten_flatten_go suffix xs acc :
-  unflattenGo (suffix ++ flattenGo xs P1) acc P1 =
-  unflattenGo suffix (xs ++ acc) P1 := by
+  unflattenGo (suffix ++ flattenGo xs P1) acc P1 = unflattenGo suffix (xs ++ acc) P1 := by
   revert suffix acc
   induction xs with
   | nil => intros suff acc; rfl
@@ -260,15 +242,13 @@ theorem unflatten_flatten_go suffix xs acc :
     rewrite [app_1_l.left_id]
     rfl
 
-theorem unflatten_flatten xs :
-  unflatten (flatten xs) = some xs := by
-  unfold flatten; unfold unflatten
+theorem unflatten_flatten {xs} : unflatten (flatten xs) = some xs := by
+  unfold flatten unflatten
   rewrite [<- (app_1_l.left_id (flattenGo xs P1))]
   rewrite [unflatten_flatten_go P1 xs [], List.append_nil]
   rfl
 
-theorem flatten_app xs ys :
-  flatten (xs ++ ys) = flatten xs ++ flatten ys := by
+theorem flatten_app {xs ys} : flatten (xs ++ ys) = flatten xs ++ flatten ys := by
   unfold flatten
   induction xs generalizing ys with
   | nil =>
@@ -283,91 +263,69 @@ theorem flatten_app xs ys :
     rewrite [<-app_assoc.assoc]
     rfl
 
-theorem flatten_cons x xs :
-  flatten (x :: xs)
-  = P1~1~0 ++ reverse (dup x) ++ flatten xs := by
-  have heq : x :: xs = [x] ++ xs := rfl
-  rw [heq]
-  apply flatten_app
+theorem flatten_cons x xs : flatten (x :: xs) = P1~1~0 ++ reverse (dup x) ++ flatten xs := by
+  rw [show x :: xs = [x] ++ xs by rfl]
+  exact flatten_app
 
-theorem flatten_suffix (l k : List Pos) :
-  l <:+ k -> ∃ q, flatten k = q ++ flatten l := by
-  rintro ⟨ l', Heq ⟩; rewrite [<- Heq]
-  exists (flatten l')
-  apply flatten_app
+theorem flatten_suffix (l k : List Pos) : l <:+ k -> ∃ q, flatten k = q ++ flatten l := by
+  rintro ⟨l', rfl⟩
+  exact ⟨_, flatten_app⟩
 
 instance app_inj (p : Pos) : Iris.Std.Injective (.++ p) where
-  inj := by
-    intros a a' Heq
-    induction p <;> simp_all [HAppend.hAppend, app]
+  inj a a' Heq := by induction p <;> simp_all [HAppend.hAppend, app]
 
 theorem reverse_involutive p : reverse (reverse p) = p := by
   induction p with
-  | xI p IH =>
-    rewrite [reverse_xI, reverse_app, IH]; rfl
-  | xO p IH =>
-    rewrite [reverse_x0, reverse_app, IH]; rfl
+  | xI p IH => rewrite [reverse_xI, reverse_app, IH]; rfl
+  | xO p IH => rewrite [reverse_x0, reverse_app, IH]; rfl
   | xH => rfl
 
 instance rev_inj : Iris.Std.Injective reverse where
-  inj := by
-    intros p q Heq
-    rewrite [<- reverse_involutive p]
-    rewrite [<- reverse_involutive q]
+  inj p q Heq := by
+    rewrite [<- reverse_involutive p, <- reverse_involutive q]
     simp [Heq]
 
 theorem dup_app p q : dup (p ++ q) = dup p ++ dup q := by
-  induction q generalizing p <;>
-  simp_all [HAppend.hAppend, app, dup]
+  induction q generalizing p <;> simp_all [HAppend.hAppend, app, dup]
 
 theorem reverse_dup (p : Pos) :
   reverse (dup p) = dup (reverse p) := by
-  have hdup := dup_app
-  have hassoc := app_assoc.assoc
-  simp at hdup hassoc
-  induction p with
-  | xI p IH =>
-    simp only [dup, reverse_xI]
-    rewrite [<- hassoc, IH, hdup]
-    rfl
-  | xO p IH =>
-    simp only [dup, reverse_x0]
-    rewrite [<- hassoc, IH, hdup]
-    rfl
-  | xH => rfl
+    induction p with
+    | xI p IH =>
+      simp only [dup, reverse_xI]
+      rewrite [<- app_assoc.assoc, IH, dup_app]
+      rfl
+    | xO p IH =>
+      simp only [dup, reverse_x0]
+      rewrite [<- app_assoc.assoc, IH, dup_app]
+      rfl
+    | xH => rfl
 
-theorem dup_suffix_eq p q s1 s2 :
+theorem dup_suffix_eq {p q s1 s2} :
   s1~1~0 ++ dup p = s2~1~0 ++ dup q -> p = q := by
   induction p generalizing q with
   | xI p IH =>
     intros Heq
     cases q <;> simp_all [HAppend.hAppend, app, dup] <;> rename Pos => q
-    rewrite [IH q] <;> rfl
+    rewrite [IH] <;> rfl
   | xO p IH =>
     intros Heq
     cases q <;> simp_all [HAppend.hAppend, app, dup] <;> rename Pos => q
-    rewrite [IH q] <;> rfl
+    rewrite [IH] <;> rfl
   | xH => cases q <;> simp [HAppend.hAppend, app, dup]
 
-theorem flatten_suffix_eq p1 p2 (xs ys : List Pos) :
-  List.length xs = List.length ys ->
-  p1 ++ flatten xs = p2 ++ flatten ys ->
-  xs = ys := by
+theorem flatten_suffix_eq {p1 p2} {xs ys : List Pos} :
+  List.length xs = List.length ys -> p1 ++ flatten xs = p2 ++ flatten ys -> xs = ys := by
   induction xs generalizing p1 p2 ys with
   | nil => simp; intros Hlen _; apply List.eq_nil_of_length_eq_zero; symm; assumption
   | cons x xs IH =>
     rcases ys with _ | ⟨ y, ys ⟩; intros Hlen _; simp [List.length] at Hlen;
-    rewrite [flatten_cons]
-    rewrite [<- app_assoc.assoc, <- app_assoc.assoc]
-    rewrite [flatten_cons]
-    rewrite [<- app_assoc.assoc, <- app_assoc.assoc]
+    repeat rewrite [flatten_cons, <- app_assoc.assoc, <- app_assoc.assoc]
     intros Hlen Hl
-    have Heq : xs = ys := by apply IH; simp_all []; apply Hl
-    rewrite [Heq]; congr; rewrite [Heq] at Hl
-    replace Hl := (app_inj (flatten ys)).inj _ _ Hl
-    rewrite [reverse_dup, reverse_dup] at Hl
-    replace Hl := (dup_suffix_eq _ _ p1 p2 Hl)
-    apply (rev_inj.inj _ _ Hl)
+    have Heq : xs = ys := IH (Nat.add_right_cancel Hlen) Hl
+    rewrite [Heq, reverse_dup, reverse_dup] at Hl
+    refine List.cons_eq_cons.mpr ⟨?_, Heq⟩
+    exact rev_inj.inj _ _ (dup_suffix_eq ((app_inj (flatten ys)).inj _ _ Hl))
 
 class Countable (A : Type) where
   encode : A -> Pos
@@ -375,29 +333,20 @@ class Countable (A : Type) where
   decode_encode x : decode (encode x) = some x
 
 instance some_inj {A} : Iris.Std.Injective (@some A) where
-  inj := by intros x y; rintro ⟨⟩; rfl
+  inj _ _ := by rintro ⟨⟩; rfl
 
 instance encode_inj [c : Countable A] : Iris.Std.Injective (c.encode) where
-  inj := by
-    intros x y Hxy; apply some_inj.inj
+  inj x _ Hxy := by
+    apply some_inj.inj
     rewrite [<- c.decode_encode x, Hxy, c.decode_encode]
     rfl
 
 instance [Countable A] : Countable (List A) where
   encode xs := Pos.flatten (List.map Countable.encode xs)
-  decode p := do
-    let positives ← (Pos.unflatten p);
-    List.mapM Countable.decode positives
-  decode_encode := by
-    intros xs
-    rewrite [Pos.unflatten_flatten]
-    rewrite [Option.bind_eq_bind, Option.bind_some, List.mapM_map]
-    induction xs with
-    | nil => rfl
-    | cons x xs IH =>
-      rewrite [List.mapM_cons, Function.comp_apply, Option.pure_def, Option.bind_eq_bind]
-      rewrite [IH, Countable.decode_encode]
-      rfl
+  decode p := (Pos.unflatten p).bind (List.mapM Countable.decode ·)
+  decode_encode xs := by
+    rewrite [Pos.unflatten_flatten, Option.bind_some, List.mapM_map]
+    induction xs with | nil => rfl | cons _ _ IH => simp [Countable.decode_encode, IH]
 
 instance : Countable Pos where
   encode := id
