8972c540a1b69e1a2f28fa83d3c42ca913a4f687
Michael Sammler <MackieLoeffel@users.noreply.github.com>
2026-04-16 09:49:02 +0200

    fix: remove duplicate printing of trace result (#315)

--- src/Iris/ProofMode/SynthInstance.lean
+++ src/Iris/ProofMode/SynthInstance.lean
@@ -80,20 +80,20 @@ partial def synthInstanceMainCore (mvar : Expr) : MetaM (Option Unit) := do
     let mvarType  ← inferType mvar
     let mvarType  ← instantiateMVars mvarType
     if !(ipmClassesExt.getState (← getEnv)).contains mvarType.getAppFn.constName then
-      return ← withTraceNode `Meta.synthInstance (return m!"{·.toTraceResult.toEmoji} switch to normal synthInstance") do
+      return ← withTraceNode `Meta.synthInstance (λ _ => return m!"switch to normal synthInstance") do
         let some e ← synthInstance? mvarType | return none
         mvar.mvarId!.assign e
         return some ()
 
     let mctx0 ← getMCtx
-    withTraceNode `Meta.synthInstance (return m!"{·.toTraceResult.toEmoji} new goal {MessageData.withMCtx mctx0 m!"{mvarType}"} => {mvarType}") do
+    withTraceNode `Meta.synthInstance (λ _ => return m!"new goal {MessageData.withMCtx mctx0 m!"{mvarType}"} => {mvarType}") do
     let instances ← SynthInstance.getInstances mvarType
     let mctx      ← getMCtx
     if instances.isEmpty then
       return none
     for inst in instances.reverse do
       let (res, match?) ← withTraceNode `Meta.synthInstance
-        (λ r => withMCtx mctx do return MessageData.withMCtx mctx m!"{(r.map (·.1)).toTraceResult.toEmoji} apply {inst.val} to {← instantiateMVars (← inferType mvar)}") do
+        (λ _ => withMCtx mctx do return MessageData.withMCtx mctx m!"apply {inst.val} to {← instantiateMVars (← inferType mvar)}") do
         setMCtx mctx
         let some (mctx', subgoals) ← withAssignableSyntheticOpaque (SynthInstance.tryResolve mvar inst) | return (none, false)
         setMCtx mctx'
@@ -123,7 +123,7 @@ def synthInstanceCore? (type : Expr) (maxResultSize? : Option Nat := none) : Met
   let opts ← getOptions
   let maxResultSize := maxResultSize?.getD (synthInstance.maxSize.get opts)
   withTraceNode `Meta.synthInstance
-    (return m!"IPM: {·.toTraceResult.toEmoji} {← instantiateMVars type}") do
+    (λ _ => return m!"IPM: {← instantiateMVars type}") do
   withConfig (fun config => { config with isDefEqStuckEx := true, transparency := TransparencyMode.instances,
                                           foApprox := true, ctxApprox := true, constApprox := false, univApprox := false }) do
   withInTypeClassResolution do
