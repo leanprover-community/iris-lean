66c68a0b2bf326038a0f653b9f958b055d0ff98a
Alvin Tang <alvin.tang@ist.ac.at>
2026-05-09 00:19:53 +0200

    Finish proof for iCombineCore

--- Iris/Iris/ProofMode/Tactics/Combine.lean
+++ Iris/Iris/ProofMode/Tactics/Combine.lean
@@ -25,7 +25,7 @@ theorem combine_singleton [BI PROP] {e e1 goal : PROP}
   (pf1 : e ⊣⊢ e1 ∗ out)
   (pf2 : e1 ∗ out ⊢ goal) : e ⊢ goal := pf1.mp.trans pf2
 
-theorem combine [BI PROP] {out1 out2 out e1 e2 goal e : PROP}
+theorem combine_step [BI PROP] {out1 out2 out e1 e2 goal e : PROP}
   (pf1 : e ⊣⊢ e1 ∗ □?b1 out1)
   (pf2 : e1 ⊣⊢ e2 ∗ □?b2 out2)
   (pf3 : e2 ∗ □?b out ⊢ goal)
@@ -41,10 +41,13 @@ theorem combine [BI PROP] {out1 out2 out e1 e2 goal e : PROP}
       _ ⊢ e2 ∗ □?b out                 := sep_mono refl (intuitionisticallyIf_mono inst.combine_sep_as)
       _ ⊢ goal                         := pf3
 
+theorem dummy_thm {p p1 p2 : Bool} : p = (p1 && p2) := sorry
+
 /-- The tactic `icombine` combines two propositions into one using the type
     class `CombineSepAs` or, by default, the separating conjunction -/
 private def iCombineCore {u} {prop : Q(Type u)} {bi e}
-  (hyps : Hyps bi e) (goal : Q($prop)) (hs : List Ident) (pat : iCasesPat) (recCall : Bool) :
+  (hyps : Hyps bi e) (goal : Q($prop)) (hs : List Ident)
+  (pat : iCasesPat) (recCall : Bool := false) :
   ProofModeM Q($e ⊢ $goal) := do
     match hs with
     -- Introduce `emp` if no hypothesis is given as `icombine` arguments
@@ -55,6 +58,7 @@ private def iCombineCore {u} {prop : Q(Type u)} {bi e}
     -- No hypothesis combined if exactly one hypothesis is given as an `icombine` argument
     | [h1] => do
       let uniq ← hyps.findWithInfo h1
+      -- The second argument to `hyps.remove` is set as `false`
       let ⟨e, hyps, out, out', p, _, pf1⟩ := hyps.remove false uniq
       let pf2 ← iCasesCore bi hyps goal pat p out' addBIGoal
       return q(combine_singleton $pf1 $pf2)
@@ -64,6 +68,10 @@ private def iCombineCore {u} {prop : Q(Type u)} {bi e}
       let uniq1 ← hyps.findWithInfo h1
       let uniq2 ← hyps.findWithInfo h2
 
+      -- The Boolean value `recCall` indicates that `iCombineCore` is currently
+      -- is called recursively. In this case, the first hypothesis in the list
+      -- is temporary and should be removed even if it is in the non-spatial
+      -- context.
       let ⟨e1, hyps1, out1, out1', p1, eq1, pf1⟩ := hyps.remove recCall uniq1
       let ⟨e2, hyps2, out2, out2', p2, eq2, pf2⟩ := hyps1.remove false uniq2
 
@@ -73,13 +81,15 @@ private def iCombineCore {u} {prop : Q(Type u)} {bi e}
       | throwError "icombine: no type class instance to combine propositions"
 
       let p := if ((p1 == q(true) && p2 == q(true))) then q(true) else q(false)
+
       match htail with
       | [] =>
         -- Introduce the new hypothesis that combines the two original hypotheses
         -- New proof goal for the tactic user
         let pf3 ← iCasesCore bi hyps2 goal pat p out addBIGoal
+        let pf4 : Q($casesOn_1 = ($p1 && $p2)) := by simpa
 
-        throwUnsupportedSyntax
+        return q(combine_step $pf1 $pf2 $pf3 $pf4 $inst)
       | htail =>
         -- Create a temporary identifier for the combined hypothesis
         let freshId ← mkFreshId
@@ -89,10 +99,10 @@ private def iCombineCore {u} {prop : Q(Type u)} {bi e}
         let newHyps := Hyps.mkSep hyps2 (Hyps.mkHyp bi freshId freshId p out)
 
         -- Add the combined proposition into the remaining list
-        let pf5 ← iCombineCore newHyps goal (h :: htail) pat True
+        let pf3 ← iCombineCore newHyps goal (h :: htail) pat true
+        let pf4 : Q($casesOn_1 = ($p1 && $p2)) := by simpa
 
-        -- Compose the two proofs: first step to new context, then recursive step to goal
-        throwUnsupportedSyntax
+        return q(combine_step $pf1 $pf2 $pf3 $pf4 $inst)
   termination_by hs.length
 
 elab "icombine" hs:(colGt ident)* "as" colGt pat:icasesPat : tactic => do
@@ -101,4 +111,5 @@ elab "icombine" hs:(colGt ident)* "as" colGt pat:icasesPat : tactic => do
 
   -- Generate new proof goals and fill in the original metavariable
   ProofModeM.runTactic λ mvar { hyps, goal, .. } => do
-    mvar.assign (← iCombineCore hyps goal hs.toList pat False)
+    let proof ← iCombineCore hyps goal hs.toList pat
+    mvar.assign proof
