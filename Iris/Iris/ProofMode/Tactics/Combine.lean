7d07a694acdddd4972d21cbb33ae24fe25a5c6cd
Alvin Tang <alvin.tang@ist.ac.at>
2026-05-10 18:45:23 +0200

    Proof for iCombineGivesCore: combine_step_gives

--- Iris/Iris/ProofMode/Tactics/Combine.lean
+++ Iris/Iris/ProofMode/Tactics/Combine.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Alvin Tang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Alvin Tang
+Authors: Michael Sammler, Alvin Tang
 -/
 module
 
@@ -14,6 +14,7 @@ namespace Iris.ProofMode
 public meta section
 open Lean Elab Tactic Meta Qq BI Std
 
+/-- Auxilary lemma for `iCombineAsCore` (base case with no hypotheses) -/
 theorem combine_as_nil [BI PROP] {e goal : PROP}
   (pf : e ∗ □ emp ⊢ goal) : e ⊢ goal := by
   calc
@@ -21,10 +22,12 @@ theorem combine_as_nil [BI PROP] {e goal : PROP}
     _ ⊢ e ∗ □ emp := sep_mono refl intuitionistically_emp.mpr
     _ ⊢ goal      := pf
 
+/-- Auxilary lemma for `iCombineAsCore` (base case with one hypothesis) -/
 theorem combine_as_singleton [BI PROP] {e e1 goal out : PROP}
   (pf1 : e ⊣⊢ e1 ∗ out)
   (pf2 : e1 ∗ out ⊢ goal) : e ⊢ goal := pf1.mp.trans pf2
 
+/-- Auxilary lemma for `iCombineAsCore` (step case with two or more hypotheses) -/
 theorem combine_as_step [BI PROP] {out1 out2 out e1 e2 e goal : PROP}
   {b1 b2 : Bool}
   (pf1 : e ⊣⊢ e1 ∗ □?b1 out1)
@@ -40,6 +43,7 @@ theorem combine_as_step [BI PROP] {out1 out2 out e1 e2 e goal : PROP}
       _ ⊢ e2 ∗ □?(b1 ∧ b2) out           := sep_mono refl (intuitionisticallyIf_mono inst.combine_sep_as)
       _ ⊢ goal                           := pf3
 
+/-- Auxilary lemma for `iCombineGivesCore` (base case with up to one hypothesis) -/
 theorem combine_nil_singleton_gives [BI PROP] {e goal : PROP}
   (pf : e ∗ □ True ⊢ goal) : e ⊢ goal := by
     calc
@@ -47,8 +51,25 @@ theorem combine_nil_singleton_gives [BI PROP] {e goal : PROP}
       _ ⊢ e ∗ □ True := sep_mono refl intuitionistically_true.mpr
       _ ⊢ goal       := pf
 
-theorem combine_step_gives [BI PROP] {out1 out2 out e goal : PROP}
-  (inst : CombineSepGives out1 out2 out) : e ⊢ goal := sorry
+/-- Auxilary lemma for `iCombineGivesCore` (step case with two or more hypotheses) -/
+theorem combine_step_gives [BI PROP] {out1 out2 out e1 e2 e goal : PROP}
+  (pf1 : e ⊣⊢ e1 ∗ out1)
+  (pf2 : e1 ⊣⊢ e2 ∗ out2)
+  (pf3 : e ∗ □ out ⊢ goal)
+  (inst : CombineSepGives out1 out2 out) : e ⊢ goal := by
+    have pf4 : e ⊣⊢ e2 ∗ out1 ∗ out2 := by calc
+      e ⊣⊢ e1 ∗ out1          := pf1
+      _ ⊣⊢ (e2 ∗ out2) ∗ out1 := sep_congr pf2 .rfl
+      _ ⊣⊢ e2 ∗ out2 ∗ out1   := sep_assoc
+      _ ⊣⊢ e2 ∗ out1 ∗ out2   := sep_congr .rfl sep_comm
+    calc
+      e ⊢ e2 ∗ out1 ∗ out2 := pf4.mp
+      _ ⊢ (e2 ∗ out1 ∗ out2) ∧ (e2 ∗ out1 ∗ out2) := and_intro refl refl
+      _ ⊢ (e2 ∗ out1 ∗ out2) ∧ (e2 ∗ <pers> out)  := and_mono refl (sep_mono refl inst.combine_sep_gives)
+      _ ⊢ (e2 ∗ out1 ∗ out2) ∧ <pers> out         := and_mono refl sep_elim_r
+      _ ⊢ (e2 ∗ out1 ∗ out2) ∗ □ out              := persistently_and_intuitionistically_sep_r.mp
+      _ ⊢ e ∗ □ out                               := sep_mono pf4.mpr refl
+      _ ⊢ goal                                    := pf3
 
 /-- The tactic `icombine` combines two propositions into one using the type
     class `CombineSepAs` or, by default, the separating conjunction -/
@@ -136,46 +157,38 @@ private def iCombineAsCore {u} {prop : Q(Type u)} {bi e}
 /-- Use the type class `CombineSepGives` to derive a new proposition in the
     the intutitionistic context and retain the original hypotheses  -/
 private def iCombineGivesCore {u} {prop : Q(Type u)} {bi e}
-  (hyps : Hyps bi e) (goal : Q($prop)) (hs : List Ident)
-  (pat : iCasesPat) (recCall : Bool := false) :
+  (hyps : Hyps bi e) (goal : Q($prop)) (hs : List Ident) (pat : iCasesPat) :
   ProofModeM Q($e ⊢ $goal) := do
     match hs with
-    -- Introduce `True` if no hypothesis is given as `icombine` arguments
-    | [] => do
-      let pf ← iCasesCore _ hyps goal pat q(true) q(iprop(True)) addBIGoal
-      return q(combine_nil_singleton_gives $pf)
-
-    -- Introduce `True` if exactly one hypothesis is given as an `icombine` argument
-    | [_] => do
-      let pf ← iCasesCore _ hyps goal pat q(true) q(iprop(True)) addBIGoal
-      return q(combine_nil_singleton_gives $pf)
-
     -- Combine the hypotheses if two or more are given as `icombine` arguments
     | h1 :: h2 :: htail => do
       let uniq1 ← hyps.findWithInfo h1
       let uniq2 ← hyps.findWithInfo h2
 
-      let ⟨_, hyps1, _, out1', p1, _, pf1⟩ := hyps.remove recCall uniq1
+      let ⟨_, hyps1, out1, _, p1, _, pf1⟩ := hyps.remove false uniq1
 
       if (h2 :: htail).contains h1 ∧ ¬isTrue p1 then
         throwError "icombine: propositions in the spatial context cannot be used as arguments multiple times"
 
-      let ⟨_, hyps2, _, out2', p2, _, pf2⟩ := hyps1.remove false uniq2
+      let ⟨_, _, out2, _, _, _, pf2⟩ := hyps1.remove false uniq2
 
       let out ← mkFreshExprMVarQ _
-      let some inst ← ProofModeM.trySynthInstanceQ q(CombineSepGives $out1' $out2' $out)
+      let some inst ← ProofModeM.trySynthInstanceQ q(CombineSepGives $out1 $out2 $out)
       -- Throw an error if a non-trivial fact is not found
       | throwError "icombine: no type class instance to combine propositions"
 
       match htail with
       | [] =>
-        -- Introduce the new hypothesis into the
+        -- Introduce the new hypothesis into the intuitionistic context
         -- New proof goal for the tactic user
         let pf3 ← iCasesCore _ hyps goal pat q(true) out addBIGoal
-        return q(combine_step_gives $inst)
+        return q(combine_step_gives $pf1 $pf2 $pf3 $inst)
 
       | _ => throwUnsupportedSyntax
-  termination_by hs.length
+    | _ => do
+      -- Introduce `True` if less than two hypotheses are given as an `icombine` argument
+      let pf ← iCasesCore _ hyps goal pat q(true) q(iprop(True)) addBIGoal
+      return q(combine_nil_singleton_gives $pf)
 
 elab "icombine" hs:(colGt ident)* "as" colGt pat:icasesPat : tactic => do
   -- Parse syntax
