3c0c808168fc41e023302549092810f65534205a
Alvin Tang <alvin.tang@ist.ac.at>
2026-05-11 13:50:48 +0200

    Update author headers

--- Iris/Iris/ProofMode/InstancesLater.lean
+++ Iris/Iris/ProofMode/InstancesLater.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Michael Sammler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Michael Sammler
+Authors: Michael Sammler, Alvin Tang
 -/
 module
 
