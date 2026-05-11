ed0845ec7e93fb7ee2c2e3d92c391912240bdc61
Alvin Tang <alvin.tang@ist.ac.at>
2026-05-11 13:50:48 +0200

    Update author headers

--- Iris/Iris/ProofMode/Classes.lean
+++ Iris/Iris/ProofMode/Classes.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Lars König. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Lars König, Michael Sammler
+Authors: Lars König, Michael Sammler, Alvin Tang
 -/
 module
 
--- Iris/Iris/ProofMode/Instances.lean
+++ Iris/Iris/ProofMode/Instances.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Lars König. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Lars König, Mario Carneiro
+Authors: Lars König, Mario Carneiro, Alvin Tang
 -/
 module
 
