1672ec69c11e06f911b716ff80520f7208a500d3
ayhon <ayhonnick@gmail.com>
2026-05-13 15:00:28 +0200

    feat: simplify `iloeb` using continuations

--- Iris/Iris/ProofMode/Tactics/Loeb.lean
+++ Iris/Iris/ProofMode/Tactics/Loeb.lean
@@ -23,9 +23,14 @@ def RevertTarget.toSelTarget : RevertTarget → SelTarget
   | .lean id => .lean id
   | .pm _ ivar => .pm ivar
 
-def ProofModeM.revertingTelescope {α} (mvar : MVarId)(g : IrisGoal)(hs : List RevertTarget) (k : MVarId → IrisGoal → ProofModeM (IrisGoal × α)) :
-    ProofModeM (IrisGoal × α) :=
-  withTraceNode `iloeb (fun _ => return s!"Reverting telescope") do
+abbrev ProofModeContinuation := ∀{u : Level}{prop: Q(Type $u)}{bi : Q(BI $prop)}{e : Q($prop)}(_hyps : Hyps bi e)(goal: Q($prop)),
+  ProofModeM Q($e ⊢ $goal)
+
+def ProofModeM.revertingTelescope
+  {u : Level}{prop: Q(Type $u)}{bi : Q(BI $prop)}{e : Q($prop)}(hyps : Hyps bi e)(goal: Q($prop))
+  (hs : List RevertTarget)
+  (k : ∀{u : Level}{prop: Q(Type $u)}{bi : Q(BI $prop)}{e : Q($prop)}(_hyps : Hyps bi e)(goal: Q($prop)), ProofModeContinuation → ProofModeM Q($e ⊢ $goal))
+   : ProofModeM Q($e ⊢ $goal) := do
   let names : List (Syntax × IntroPat) ← hs.mapM fun
     | .lean id => do
       let name ← Lean.mkIdent <$> id.getUserName
@@ -35,31 +40,11 @@ def ProofModeM.revertingTelescope {α} (mvar : MVarId)(g : IrisGoal)(hs : List R
       let name ← Lean.mkIdent <$> (hyps.getUserName? ivar).getM
       let ident ← `(binderIdent| $name:ident)
       return (name, .intro <| (if persistent? then .intuitionistic else id) <| .one ident)
+  trace[iloeb] s!"Calling reverting telescope with {names.map (·.1)} on context {←ppExpr hyps.tm}\n⊢\n{←ppExpr goal}"
 
-  -- revert hypothesis in `hs`
-  let (g, mvar) ← withTraceNode `iloeb (fun _ => return m!"Reverting hypothesis {names.map (·.1.getId)}") do
-    trace[iloeb] s!"Before revert: {← (liftMetaM ∘ ppExpr =<< inferType (.mvar mvar))}"
-    let (g, expr) ← iRevertCore mvar g (hs.map RevertTarget.toSelTarget)
-    ProofModeM.pruneSolvedGoals
-    trace[iloeb] s!"After revert: {← (←ProofModeM.getUnsolvedGoals).mapM (liftMetaM ∘ ppExpr =<< MVarId.getType ·)}"
-    trace[iloeb] s!"Revert returned: {←ppExpr expr}"
-    pure (g, expr.mvarId!)
-
-  -- Run tactic
-  let ({hyps, goal, ..}, res) ← withTraceNode `iloeb (fun _ => return s!"Running tactic") do
-    let (g, res) ← k mvar g
-    ProofModeM.pruneSolvedGoals
-    pure (g, res)
-
-  -- introduce back the hypethesis in `hs`
-  withTraceNode `iloeb (fun _ => return m!"Re-introducing hypothesis with {names.map (·.1.getId)}") do
-    trace[iloeb] s!"Before intros: {← (←ProofModeM.getUnsolvedGoals).mapM (liftMetaM ∘ ppExpr =<< MVarId.getType ·)}"
-    let mvar ← getMainGoal
-    let expr ← iIntroCore hyps goal names
-    ProofModeM.pruneSolvedGoals
-    mvar.assign expr
-    trace[iloeb] s!"After intros: {← (←ProofModeM.getUnsolvedGoals).mapM (liftMetaM ∘ ppExpr =<< MVarId.getType ·)}"
-  return (g, res)
+  iRevertCore (hs.map RevertTarget.toSelTarget) hyps goal fun hyps goal => do
+  k hyps goal fun hyps goal => do
+  iIntroCore hyps goal names
 
 /--
   Apply Löb induction in the current goal.
@@ -75,7 +60,7 @@ syntax (name := iloeb) "iloeb " " as " binderIdent (" generalizing " (ppSpace co
 @[inherit_doc iloeb]
 elab_rules : tactic
 | `(tactic| iloeb as $IH:binderIdent $[generalizing $[$hs:selPat]*]? ) => do
-  ProofModeM.runTactic fun mvid g@{hyps, ..} => do
+  ProofModeM.runTactic fun mvid {hyps, goal, ..} => do
     let spatialCtx := hyps.spatialIVarIds.map (RevertTarget.pm false)
     let generalizedHs ← do
       let hs := hs.getD #[]
@@ -85,40 +70,24 @@ elab_rules : tactic
         |>.filterMapM fun
           | (.pm ivar, ref) => do
             if spatialCtx.contains (.pm false ivar) then
-              logWarningAt ref m!"Spatial hypothesis are generalized automatically"
+              logWarningAt ref m!"Spatial hypothesis are generalized automatically by iloeb"
               return none
             else
               return some (RevertTarget.pm true ivar)
           | (.lean id, _) => return some (.lean id)
 
-    let _ ← ProofModeM.revertingTelescope mvid g (generalizedHs ++ spatialCtx) fun mvid {u, prop, goal, hyps, ..} => do
-      trace[iloeb] s!"Goals before tactic : {←(liftM ∘ ppExpr =<< MVarId.getType (←ProofModeM.getMainGoal))}"
-      let x : Term := ←`(term| @BI.loeb_wand_intuitionistically _)
-      let ⟨_, hyps', p, out, pf⟩ ← iHave hyps ⟨x, []⟩ true
-      let pf' ← iApplyCore hyps' p out goal
-      mvid.assign q($(pf).trans $pf')
-      trace[iloeb] s!"Goals after applying iloeb: {←(liftM ∘ ppExpr =<< MVarId.getType (←ProofModeM.getMainGoal))}"
-
-      let .some biloeb ← ProofModeM.trySynthInstanceQ q(BI.BILoeb $prop)
+    let expr ← ProofModeM.revertingTelescope hyps goal (generalizedHs ++ spatialCtx) fun {u prop _ _} hyps goal k => do
+      let .some _ ← ProofModeM.trySynthInstanceQ q(BI.BILoeb $prop)
         | throwError m!"Cannot use `iloeb` if there is no `{←ppExpr q(BI.BILoeb $prop)}` instance available"
-      (←ProofModeM.getMainGoal).assign biloeb
-      trace[iloeb] s!"Goals after BILoeb condition dispatch: {← ((←ProofModeM.getUnsolvedGoals).mapM (liftM ∘ ppExpr =<< MVarId.getType ·))}\n{repr (←ProofModeM.getUnsolvedGoals)}"
+      let pf := q(BI.loeb_wand_intuitionistically (P := $goal))
+      let pf' ← do
+        -- We have applied BI.loeb_wand_intuitionistically
+        let goal := q(iprop(□ (□ ▷ $goal -∗ $goal)))
+        iModIntroCore hyps goal (← `(_)) fun hyps goal => do
+        iIntroCore hyps goal [(IH, .intro <| .intuitionistic <| .one IH)] fun hyps goal => do
+        k hyps goal
+      return q($(pf').trans $pf)
 
-      let pf ← iModIntroCore hyps' q(iprop(□ (□ ▷ $goal -∗ $goal))) (← `(_)) fun hyps goal => do
-        addBIGoal hyps goal
-      let imodGoalId ← ProofModeM.getMainGoal
-      imodGoalId.assign pf
-      trace[iloeb] s!"Goals after imodintro: {← ((←ProofModeM.getUnsolvedGoals).mapM (liftM ∘ ppExpr =<< MVarId.getType ·))}\n{repr (←ProofModeM.getUnsolvedGoals)}"
-
-      let introGoalId ← ProofModeM.getMainGoal
-      let some {goal, hyps, ..} := parseIrisGoal? (←introGoalId.getType)
-        | throwError "Expected resulting goal to be an Iris goal"
-      let expr ← iIntroCore hyps goal [(IH, .intro <| .intuitionistic <| .one IH)]
-      introGoalId.assign expr
-      trace[iloeb] s!"Goals after introducing {IH}: {← ((←ProofModeM.getUnsolvedGoals).mapM (liftM ∘ ppExpr =<< MVarId.getType ·))}\n{repr (←ProofModeM.getUnsolvedGoals)}"
-
-      let some g := parseIrisGoal? (←MVarId.getType <| ←ProofModeM.getMainGoal)
-        | throwError "Expected resulting goal to be an Iris goal"
-      return ⟨g, ()⟩
+    mvid.assign expr
 
 initialize registerTraceClass `iloeb
