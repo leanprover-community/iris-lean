7c20b3cb1a6b50a48e83a48f4732b243b30acffb
Alvin Tang <alvin.tang@ist.ac.at>
2026-05-22 13:48:56 +0200

    Reorganise IsOp type classes

--- Iris/Iris/Algebra/IsOp.lean
+++ Iris/Iris/Algebra/IsOp.lean
@@ -16,56 +16,65 @@ open CMRA
 
 section IsOp
 
-
-
-@[rocq_alias IsOp]
+/--
+  A type class that allows merging `b1` and `b2` into `a` as well as
+  to split `a` into `b1` and `b2`.
+-/
+@[rocq_alias IsOp']
 class IsOp [CMRA α] (a b1 b2 : α) where
   is_op : a ≡ b1 • b2
+
+/--
+  A type class for merging `b1` and `b2` into `a`.
+-/
+@[rocq_alias IsOp]
+class IsOpMerge [CMRA α] (a : outParam α) (b1 b2 : α) extends IsOp a b1 b2
 
 /--
   Merging with `•` should have the lowest priority.
 -/
 @[rocq_alias is_op_op]
-instance (priority := default - 100) isOp_op [CMRA α] (a b : α) : IsOp (a • b) a b where
+instance (priority := default - 100) isOp_op [CMRA α] (a b : α) :
+    IsOpMerge (a • b) a b where
   is_op := .rfl
-
-/--
-  A type class for merging `b1` and `b2` into `a`.
--/
-@[rocq_alias IsOp']
-class IsOpMerge [CMRA α] (a : outParam α) (b1 b2 : α) where
-  is_op : a ≡ b1 • b2
 
 /--
   A type class for splitting `a` into `b1` and `b2`.
 -/
-class IsOpSplit [CMRA α] (a : α) (b1 b2 : outParam α) where
-  is_op : a ≡ b1 • b2
+@[rocq_alias IsOp'LR]
+class IsOpSplit [CMRA α] (a : α) (b1 b2 : outParam α) extends IsOp a b1 b2
 
+/--
+  Splitting with `•` should have the highest priority.
+-/
 @[rocq_alias is_op_lr_op]
 instance (priority := default + 100) isOpSplit_op [CMRA α] (a b : α) :
     IsOpSplit (a • b) a b where
   is_op := .rfl
 
-/- The following type class instances were originally defined with `IsOp'`. -/
+/-
+  The following type class instances were originally defined for `IsOp'`.
+  The distinction between `IsOp` and `IsOp'` is no longe relevant in Lean,
+  so the following instances all concern `IsOp`.
+-/
 
 @[rocq_alias is_op_pair]
-instance isOp_pair [CMRA α] (a b1 b2 : α) (a' b1' b2' : α)
+instance isOpMerge_pair [CMRA α] (a b1 b2 : α) (a' b1' b2' : α)
     [h1 : IsOp a b1 b2] [h2 : IsOp a' b1' b2'] : IsOp (a, a') (b1, b1') (b2, b2') where
   is_op := ⟨h1.is_op, h2.is_op⟩
 
 @[rocq_alias is_op_pair_core_id_l]
-instance isOp_pair_core_id_l [CMRA α] [CMRA β] (a : α) (a' b1' b2' : β)
+instance isOpMerge_pair_core_id_l [CMRA α] [CMRA β] (a : α) (a' b1' b2' : β)
     [h1 : CoreId a] [h2 : IsOp a' b1' b2'] : IsOp (a, a') (a, b1') (a, b2') where
   is_op := ⟨(op_self a).symm, h2.is_op⟩
 
 @[rocq_alias is_op_pair_core_id_r]
-instance isOp_pair_core_id_r [CMRA α] [CMRA β] (a b1 b2 : α) (a' : β)
+instance isOpMerge_pair_core_id_r [CMRA α] [CMRA β] (a b1 b2 : α) (a' : β)
     [h1 : CoreId a'] [h2 : IsOp a b1 b2] : IsOp (a, a') (b1, a') (b2, a') where
   is_op := ⟨h2.is_op, (op_self a').symm⟩
 
 @[rocq_alias is_op_Some]
-instance isOp_some [CMRA α] (a b1 b2 : α)
+instance isOpMerge_some [CMRA α] (a b1 b2 : α)
     [h : IsOp a b1 b2] : IsOp (some a) (some b1) (some b2) where
   is_op := h.is_op
 
