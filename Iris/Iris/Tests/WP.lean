1aeb18868a5de15f731750ea418063c44d2b3b2b
Michael Sammler <noreply@sammler.me>
2026-05-22 16:47:15 +0200

    fix: make A an outParam of Wp and TotalWp
    This reduces the need for type annotations and there should be at most
    one Wp instance per Expr.

--- Iris/Iris/Tests/WP.lean
+++ Iris/Iris/Tests/WP.lean
@@ -16,13 +16,11 @@ open Iris
 section TestWP
 set_option linter.unusedVariables false
 
-variable (PROP Expr : Type _) (Val : outParam (Type _)) (A : Type _)
-variable [Wp PROP Expr Val A]
+variable (PROP Expr : Type _) (Val : Type _)
 variable [Wp PROP Expr Val Stuckness]
-variable [TotalWP PROP Expr Val A]
-variable [TotalWP PROP Expr Val Stuckness]
+variable [TotalWp PROP Expr Val Stuckness]
 
-variable (e : Expr) (s : A) (E : CoPset)
+variable (e : Expr) (s : Stuckness) (E : CoPset)
 
 -- Base no-binder cases
 variable (Φ : Val → PROP)
