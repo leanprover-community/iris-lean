abb54acd4d8eee3ebe1b0eca57e6d852a1588732
Zongyuan Liu <liuzongyuan0505@gmail.com>
2026-05-26 17:14:32 +0200

    Add name

--- Iris/Iris/ProgramLogic/Adequacy.lean
+++ Iris/Iris/ProgramLogic/Adequacy.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Haokun Li. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Haokun Li, Sergei Stepanenko
+Authors: Haokun Li, Sergei Stepanenko, Zongyuan Liu
 -/
 module
 
