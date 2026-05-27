a1637f1242c94cb9513ef9c6ad520e9545fe5179
Sergei Stepanenko <sergei.stepanenko@cs.au.dk>
2026-05-27 12:36:22 +0200

    pass over lemmas

--- Iris/Iris/Instances/Lib/GhostMap.lean
+++ Iris/Iris/Instances/Lib/GhostMap.lean
@@ -14,23 +14,23 @@ public import Iris.BI.BigOp.BigOp
 
 namespace Iris
 
-open Iris Std HeapView PartialMap Iris.Algebra
+open Iris Std HeapView PartialMap Iris.Algebra CMRA BI
 
 class GhostMapG (GF : BundledGFunctors) (F: outParam (Type _))
     (K V: Type _)(H : outParam <| Type _ → Type _)
-    [UFraction F][LawfulPartialMap H K] where
+    [UFraction F] [LawfulFiniteMap H K] where
   elem: ElemG GF (constOF (HeapView F K (Agree (LeibnizO V)) H))
 
 section definitions
 
-variable [UFraction F][LawfulPartialMap H K][hgm: GhostMapG GF F K V H]
+variable [UFraction F][LawfulFiniteMap H K][hgm: GhostMapG GF F K V H]
 
 public def ghost_map_auth (γ : GName) (dq : DFrac F) (m : H V): IProp GF :=
   iOwn (E := hgm.elem) γ
-    (HeapView.Auth dq (Iris.Std.PartialMap.map (fun x ↦ toAgree ⟨x⟩) m))
+    (Auth dq (Iris.Std.PartialMap.map (fun x ↦ toAgree ⟨x⟩) m))
 
 public def ghost_map_elem (γ : GName) (dq : DFrac F) (k: K) (v: V): IProp GF :=
-  iOwn (E := hgm.elem) γ (HeapView.Frag k dq (toAgree ⟨v⟩))
+  iOwn (E := hgm.elem) γ (Frag k dq (toAgree ⟨v⟩))
 
 end definitions
 
@@ -41,32 +41,30 @@ notation γ " ↪◯MAP[" k "] " v => ghost_map_elem γ (DFrac.own 1) k v
 
 section lemmas
 
-variable (F K V : Type _) (H : Type _ → Type _) [UFraction F] [lpm: LawfulPartialMap H K]
+variable (F K V : Type _) (H : Type _ → Type _) [UFraction F] [LawfulFiniteMap H K]
 variable [hgm: GhostMapG GF F K V H]
 
 @[rocq_alias ghost_map_elem_timeless]
-instance (γ : GName)(k: K)(dq: DFrac F)(v: V): BI.Timeless (PROP := IProp GF) (γ ↪◯MAP[k]{dq} v) :=
+instance (γ : GName)(k: K)(dq: DFrac F)(v: V): Timeless (PROP := IProp GF) (γ ↪◯MAP[k]{dq} v) :=
   iOwn_timeless (E := hgm.elem)
 
 @[rocq_alias ghost_map_elem_persistent]
-instance (γ : GName)(k: K)(v: V): BI.Persistent (PROP := IProp GF) (γ ↪◯MAP[k]{.discard} v) := by
+instance (γ : GName)(k: K)(v: V): Persistent (PROP := IProp GF) (γ ↪◯MAP[k]{.discard} v) := by
   unfold ghost_map_elem
-  exact instPersistentIPropIOwnOfCoreIdAp (E := hgm.elem)
+  infer_instance
 
 @[rocq_alias ghost_map_elem_fractional]
 instance ghost_map_elem_fractional (γ : GName)(k: K)(v: V)
     : Fractional (PROP := IProp GF) (fun q: F => γ ↪◯MAP[k]{.own q} v) where
   fractional p q := by
+    -- If using `refine`/`apply` instead of stating facts with `have`,
+    -- it's possible to eliminate some of implicit annotations.
     unfold ghost_map_elem
-    let ta := @toAgree (LeibnizO V) { car := v }
-    have :
-        Frag (H := H) k (DFrac.own (p + q)) (ta • ta) ≡
-        Frag k (DFrac.own (p + q)) ta
-      := OFE.NonExpansive.eqv Iris.Agree.idemp
-    have := frag_add_op_equiv.symm.trans this
-    have := (@iOwn_ne GF _ _ GhostMapG.elem γ).eqv this
-    have := (BI.equiv_iff (PROP := IProp GF)).mp this
-    exact this.symm.trans <| iOwn_op (E := hgm.elem)
+    refine .trans ?_ (iOwn_op (E := hgm.elem))
+    refine equiv_iff.mp ?_
+    refine .trans ?_ ((iOwn_ne (E := hgm.elem)).eqv frag_add_op_equiv)
+    refine OFE.NonExpansive.eqv (OFE.NonExpansive.eqv ?_)
+    exact Agree.idemp.symm
 
 @[rocq_alias ghost_map_elem_as_fractional]
 instance (γ : GName) (k: K) (v: V)
@@ -76,10 +74,18 @@ instance (γ : GName) (k: K) (v: V)
   as_fractional_fractional := ghost_map_elem_fractional F K V H γ k v
 
 @[rocq_alias ghost_map_elems_unseal]
-theorem ghost_map_elems_unseal [LawfulFiniteMap H K] γ (m : H V) dq :
+theorem ghost_map_elems_unseal γ (m : H V) dq :
   ([∗map] k ↦ v ∈ m, γ ↪◯MAP[k]{dq} v) ==∗
-  iOwn (E := hgm.elem) γ ([^ CMRA.op map] k ↦ v ∈ m,
-    Frag (V:= Agree (LeibnizO V)) k dq (toAgree { car := v })) := sorry
+  iOwn (E := hgm.elem) γ ([^ op map] k ↦ v ∈ m,
+    Frag k dq (toAgree ⟨v⟩)) := by
+  iintro H
+  by_cases h : m = ∅
+  · subst h
+    simp only [BigOpM.bigOpM_empty]
+    iapply iOwn_unit (E := hgm.elem)
+  · -- Either via `BigOpM.bigOpM_weak_hom` or wait a bit, until there are `iOwn_bigOp_*` lemmas.
+    -- Zongyuan kindly agreed to port them.
+    sorry
 
 @[rocq_alias ghost_map_elem_valid]
 theorem ghost_map_elem_valid (γ : GName) (k : K) (dq: DFrac F) (v: V) :
@@ -98,21 +104,25 @@ theorem ghost_map_elem_valid (γ : GName) (k : K) (dq: DFrac F) (v: V) :
 theorem ghost_map_elem_valid_2 (γ : GName) (k : K) (dq1: DFrac F) (dq2: DFrac F) (v1: V) (v2: V) :
     (γ ↪◯MAP[k]{dq1} v1) ∗ (γ ↪◯MAP[k]{dq2} v2) ⊢@{IProp GF}
       internalCmraValid (dq1 • dq2) ∧ ⌜v1 = v2⌝ := by
-  refine (iOwn_op (E := hgm.elem)).mpr.trans ?_
-  refine (iOwn_cmraValid (E := hgm.elem)).trans ?_
-  iintro h
-  ipure h; ipure_intro
-  have ⟨vdq, va⟩ := frag_op_valid_iff.mp h
+  -- To use `iOwn_*` family of lemmas in the proof mode,
+  -- the wrappers should be first unfolded (or be abbrevs, but definitions are probably better).
+  unfold ghost_map_elem
+  iintro H
+  icases iOwn_op (E := hgm.elem) $$ H with H
+  icases iOwn_cmraValid (E := hgm.elem) $$ H with %H
+  obtain ⟨vdq, va⟩ := frag_op_valid_iff.mp H
+  ipure_intro
   exact ⟨vdq, congrArg (·.car) (toAgree_op_valid_iff_eq.mp va)⟩
 
 @[rocq_alias ghost_map_elem_agree]
 theorem ghost_map_elem_agree (γ : GName) (k : K) (dq1 : DFrac F) (dq2 : DFrac F) (v1 : V) (v2 : V) :
     (γ ↪◯MAP[k]{dq1} v1) ∗ (γ ↪◯MAP[k]{dq2} v2) ⊢@{IProp GF} ⌜v1 = v2⌝ := by
-  let := ghost_map_elem_valid_2 (GF := GF) F K V H γ k dq1 dq2 v1 v2
-  refine this.trans ?_
-  iintro h
-  ipure h; ipure_intro
-  exact h.right
+  -- Notation difference with the Rocq version: if you have a hypothesis of shape `H : A ∧ B`,
+  -- in Iris you can't derive both `H1 : A` and `H2 : B` at the same time,
+  -- `⟨-, T⟩` destruction pattern allows to drop the first one
+  -- `$` tries to iframe it immediately.
+  iintro H
+  icases ghost_map_elem_valid_2 $$ H with ⟨-, $⟩
 
 -- Global Instance ghost_map_elem_combine_gives γ k v1 dq1 v2 dq2 :
 --   CombineSepGives (γ ↪◯MAP[k]{dq1} v1) (γ ↪◯MAP[k]{dq2} v2)
@@ -128,14 +138,20 @@ theorem ghost_map_elem_agree (γ : GName) (k : K) (dq1 : DFrac F) (dq2 : DFrac F
 theorem ghost_map_elem_combine (γ : GName) (k : K) (dq1 : DFrac F) (dq2 : DFrac F) (v1 : V) (v2 : V) :
     (γ ↪◯MAP[k]{dq1} v1) ∗ (γ ↪◯MAP[k]{dq2} v2)
     ⊢@{IProp GF} (γ ↪◯MAP[k]{dq1 • dq2} v1) ∗ ⌜v1 = v2⌝ := by
+  iintro H
+  -- If the conclusion of icases is persistent/plain/pure, it is possible to
+  -- get the result without losing the input hypothesis.
+  icases ghost_map_elem_agree $$ H with #%heq
+  iframe %heq; subst heq
+  -- Here it is indeed a bit annoying to work with rewriting.
+  -- Perhaps, there's a better way.
   unfold ghost_map_elem
-  refine (iOwn_op (E := hgm.elem)).mpr.trans ?_
-  have : (Frag (H := H) k dq1 (toAgree ({ car := v1 } : LeibnizO V)) •
-    Frag k dq2 (toAgree ({ car := v2 } : LeibnizO V))) ≡ _ := frag_op_equiv.symm
-  have := (@iOwn_ne GF _ _ GhostMapG.elem γ).eqv this
-  have := (BI.equiv_iff (PROP := IProp GF)).mp this
-  -- TODO (life is painful without rewriting)
-  sorry
+  icases iOwn_op (E := hgm.elem) $$ H with H
+  iapply iOwn_mono (E := hgm.elem) $$ H
+  refine inc_of_inc_of_eqv ?_ frag_op_equiv
+  refine inc_of_inc_of_eqv .rfl ?_
+  refine OFE.NonExpansive.eqv ?_
+  exact Agree.idemp.symm
 
 -- Global Instance ghost_map_elem_combine_as (γ : GName) (k : K) (dq1 : DFrac F) (dq2 : DFrac F) (v1 : V) (v2 : V) :
 --   CombineSepAs (γ ↪◯MAP[k]{dq1} v1) (γ ↪◯MAP[k]{dq2} v2)
@@ -145,20 +161,14 @@ theorem ghost_map_elem_combine (γ : GName) (k : K) (dq1 : DFrac F) (dq2 : DFrac
 theorem ghost_map_elem_frac_ne γ (k1 : K) (k2 : K) (dq1 : DFrac F) (dq2 : DFrac F) (v1 : V) (v2 : V) :
    ¬ ✓ (dq1 • dq2) →
      (γ ↪◯MAP[k1]{dq1} v1) ∗ (γ ↪◯MAP[k2]{dq2} v2) ⊢@{IProp GF} ⌜k1 ≠ k2⌝ := by
-  let foo := ghost_map_elem_valid_2 (GF := GF) F K V H γ k1 dq1 dq2 v1 v2
-  intro nv
-  let bar : @BI.Entails (IProp GF) UPred.instBIBaseUPred
-      iprop(internalCmraValid (dq1 • dq2) ∧ ⌜v1 = v2⌝) iprop(⌜k1 ≠ k2⌝) := by
-    iintro h
-    ipure h; ipure_intro
-    grind
-  have := foo.trans bar
-  if hk: k1 = k2 then
-    exact hk ▸ this
-  else
-    iintro _
-    ipure_intro
-    exact hk
+  iintro %Hcontra H
+  by_cases Heq : k1 = k2
+  · subst Heq
+    -- Same here, patterns can help a lot, while allowing to stay in the proof mode
+    -- for longer, which in turn helps with implicit arguments.
+    icases ghost_map_elem_valid_2 $$ H with ⟨%G, -⟩
+    exact (Hcontra G).elim
+  · ipure_intro; exact Heq
 
 @[rocq_alias ghost_map_elem_ne]
 theorem ghost_map_elem_ne γ (k1 : K) (k2 : K) (dq2 : DFrac F) (v1 : V) (v2 : V) :
@@ -175,7 +185,7 @@ theorem ghost_map_elem_unpersist (γ : GName) (k : K) (v : V) :
 -- * lemmas about [ghost_map_auth]
 
 @[rocq_alias ghost_map_alloc_strong]
-theorem ghost_map_alloc_strong [LawfulFiniteMap H K] (P : GName → Prop) (m : H V) :
+theorem ghost_map_alloc_strong (P : GName → Prop) (m : H V) :
   (hP : ∀ N, ∃ k, N ≤ k ∧ P k) →
   ⊢@{IProp GF} |==> ∃ γ, ⌜P γ⌝ ∗ (γ ↪●MAP m) ∗ [∗map] k ↦ v ∈ m, γ ↪◯MAP[k] v := sorry
 
@@ -185,9 +195,9 @@ theorem ghost_map_alloc_strong_empty (P : GName → Prop) :
   ⊢@{IProp GF} |==> ∃ γ, ⌜P γ⌝ ∗ (γ ↪●MAP (∅ : H V)) := sorry
 
 @[rocq_alias ghost_map_alloc]
-theorem ghost_map_alloc [LawfulFiniteMap H K](m: H V) :
+theorem ghost_map_alloc (m : H V) :
     ⊢@{IProp GF} |==> ∃ γ, (γ ↪●MAP m) ∗
-      [∗map] k ↦ v ∈ m, γ ↪◯MAP[k]{(.own 1 : DFrac F)} v := sorry
+      [∗map] k ↦ v ∈ m, γ ↪◯MAP[k] v := sorry
 
 @[rocq_alias ghost_map_alloc_empty]
 theorem ghost_map_alloc_empty :
@@ -257,31 +267,31 @@ theorem ghost_map_update {γ} {m: H V} (k: K) (v: V) (w: V) :
   ⊢@{IProp GF} (γ ↪●MAP m) -∗ (γ ↪◯MAP[k] v) ==∗ (γ ↪●MAP insert m k w) ∗ γ ↪◯MAP[k] w := sorry
 
 --  Big-op versions of above lemmas
-theorem ghost_map_lookup_big [LawfulFiniteMap H K] {γ dq} {m : H V} {dq'} m0 :
+theorem ghost_map_lookup_big {γ dq} {m : H V} {dq'} m0 :
   ⊢@{IProp GF} (γ ↪●MAP{dq} m) -∗
   ([∗map] k ↦ v ∈ m0, γ ↪◯MAP[k]{dq'} v) -∗
   ⌜m0 ⊆ m⌝ := sorry
 
 @[rocq_alias ghost_map_insert_big]
-theorem ghost_map_insert_big [LawfulFiniteMap H K] {γ m} (m' : H V) :
+theorem ghost_map_insert_big {γ m} (m' : H V) :
   (m' ##ₘ m) →
   ⊢@{IProp GF} (γ ↪●MAP m) ==∗
   (γ ↪●MAP (m' ∪ m)) ∗ [∗map] k ↦ v ∈ m', γ ↪◯MAP[k] v := sorry
 
 @[rocq_alias ghost_map_insert_persist_big]
-theorem ghost_map_insert_persist_big [LawfulFiniteMap H K] {γ m} (m' : H V) :
+theorem ghost_map_insert_persist_big {γ m} (m' : H V) :
   m' ##ₘ m →
   ⊢@{IProp GF} (γ ↪●MAP m) ==∗
   (γ ↪●MAP (m' ∪ m)) ∗ [∗map] k ↦ v ∈ m', γ ↪◯MAP[k]{.discard} v := sorry
 
 @[rocq_alias ghost_map_delete_big]
-theorem ghost_map_delete_big [LawfulFiniteMap H K] {γ m} (m0 : H V) :
+theorem ghost_map_delete_big {γ m} (m0 : H V) :
   ⊢@{IProp GF} (γ ↪●MAP m) -∗
   ([∗map] k ↦v ∈ m0, γ ↪◯MAP[k] v) ==∗
   (γ ↪●MAP (m \ m0)) := sorry
 
 @[rocq_alias ghost_map_update_big]
-theorem ghost_map_update_big [LawfulFiniteMap H K] {γ m} (m0 m1 : H V) :
+theorem ghost_map_update_big {γ m} (m0 m1 : H V) :
   dom m0 = dom m1 →
   ⊢@{IProp GF} (γ ↪●MAP m) -∗
   ([∗map] k ↦v ∈ m0, γ ↪◯MAP[k] v) ==∗
