35407d538172edc82b76fa7c4c77b6a3a82f3b81
Alvin Tang <alvin.tang@ist.ac.at>
2026-06-22 11:17:38 +0200

    Introduce assumptions into `tac_inv_elim`

--- Iris/Iris/ProofMode/Tactics/Inv.lean
+++ Iris/Iris/ProofMode/Tactics/Inv.lean
@@ -19,7 +19,19 @@ public meta section
 open Lean Elab Tactic Meta Qq BI Std
 
 @[rocq_alias tac_inv_elim]
-theorem tac_inv_elim [BI PROP] {e goal : PROP} : e ⊢ goal := sorry
+theorem tac_inv_elim [BI PROP] {e e' goal : PROP} {ϕ : Prop} {X : Type} {p : Bool}
+    {Pinv Pin : PROP} {mPclose : Option <| X → PROP} {Pout Q' : X → PROP}
+    (inst : ElimInv ϕ X Pinv Pin Pout mPclose goal Q')
+    (hϕ : ϕ)
+    (hAcc : ∀ x, e' ⊢ Pout x -∗ Q' x)
+    (pf : e ⊣⊢ e' ∗ □?p Pinv) :
+    e ⊢ goal := by
+  apply pf.mp.trans
+  have h := inst.elim_inv hϕ
+  clear inst hϕ pf
+  cases mPclose with simp_all
+  | none => sorry
+  | some mPclose => sorry
 
 private def iInvCore {u} {prop : Q(Type u)} {bi e} (hyps : Hyps bi e) (goal : Q($prop))
     (ivar : IVarId)
@@ -28,7 +40,7 @@ private def iInvCore {u} {prop : Q(Type u)} {bi e} (hyps : Hyps bi e) (goal : Q(
     -- (hclose : Option <| TSyntax `ident) :
     ProofModeM Q($e ⊢ $goal) := do
   -- Find the hypothesis from the context
-  let ⟨e', hyps', _, ty, _, _ , _⟩ := hyps.remove false ivar
+  let ⟨e', hyps', _, Pinv, _, _ , pf⟩ := hyps.remove false ivar
 
   let ϕ ← mkFreshExprMVarQ q(Prop)
   let Pin ← mkFreshExprMVarQ q($prop)
@@ -37,23 +49,24 @@ private def iInvCore {u} {prop : Q(Type u)} {bi e} (hyps : Hyps bi e) (goal : Q(
   let mPclose ← mkFreshExprMVarQ q(Option ($X → $prop))
   let Q' ← mkFreshExprMVarQ q($X → $prop)
 
-  let some inst ← ProofModeM.trySynthInstanceQ q(ElimInv $ϕ $X $ty $Pin $Pout $mPclose $goal $Q')
-  | throwError "iinv: ElimInv type class synthesis error with goal {goal}"
-
   let X    : Q(Type)       ← instantiateMVars X
   let Pout : Q($X → $prop) ← instantiateMVars Pout
   let Q'   : Q($X → $prop) ← instantiateMVars Q'
+  let mPclose : Q(Option ($X → $prop)) ← instantiateMVars mPclose
+
+  let some inst ← ProofModeM.trySynthInstanceQ q(ElimInv $ϕ $X $Pinv $Pin $Pout $mPclose $goal $Q')
+  | throwError "iinv: ElimInv type class synthesis error with goal {goal}"
 
   let hϕ ← iSolveSidecondition q($ϕ) false
 
   let hAcc : Q(∀ x, $e' ⊢ $Pout x -∗ $Q' x) ←
     withLocalDeclDQ (← mkFreshUserName `x) X fun x => do
       let poutX : Q($prop) := Expr.headBeta q($Pout $x)
-      let qX    : Q($prop) := Expr.headBeta q($Q' $x)
+      let qX : Q($prop) := Expr.headBeta q($Q' $x)
       let body ← iIntroCore hyps' q(iprop($poutX -∗ $qX)) [introPat]
       mkLambdaFVars #[x] body
 
-  return q(tac_inv_elim)
+  return q(tac_inv_elim $inst $hϕ $hAcc $pf)
 
 elab "iinv " h:ident " as " ipat:introPat : tactic => do
   -- Parse the introduction pattern used for destructing the result
