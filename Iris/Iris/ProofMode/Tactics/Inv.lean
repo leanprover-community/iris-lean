7d818c368bad901b5df7b66fec6c6f41f98e8053
Alvin Tang <alvin.tang@ist.ac.at>
2026-06-25 17:29:54 +0200

    Towards restructuring `iInvCore` and `tac_inv_elim` to use `iCasesCore` instead of `iIntroCore`

--- Iris/Iris/ProofMode/Tactics/Inv.lean
+++ Iris/Iris/ProofMode/Tactics/Inv.lean
@@ -21,46 +21,36 @@ open Lean Elab Tactic Meta Qq BI Std
 def optionMap {PROP : Type u} {X : Type} (mP : Option (X → PROP)) (x : X) : Option PROP :=
   mP.map (· x)
 
-@[rocq_alias tac_inv_elim]
 theorem tac_inv_elim [BI PROP]
     {e e' e'' goal : PROP} {ϕ : Prop} {X : Type} {p close : Bool}
     {Pinv Pin : PROP} {mPclose : Option <| X → PROP} {Pout Q' : X → PROP}
     (inst : ElimInv ϕ X Pinv Pin Pout close mPclose goal Q')
     (hϕ : ϕ)
-    (hAcc : ∀ x, e'' ⊢ Pout x -∗ mPclose.map (· x) -∗? Q' x)
+    (hAcc : ∀ x, e'' ∗ Pout x ∗ ((mPclose.map (· x)).getD emp) ⊢ Q' x)
     (pf : e ⊣⊢ e' ∗ □?p Pinv)
     (pfPin : e' ∗ (Pin -∗ Pin) ⊢ e'' ∗ Pin) :
-    e ⊢ goal := by
-  have h0 := inst.elim_inv hϕ
-  have h1 : e ⊢ Pinv ∗ Pin ∗ e'' := calc
-    e ⊢ e' ∗ □?p Pinv            := pf.mp
-    _ ⊢ □?p Pinv ∗ e'            := sep_comm.mp
-    _ ⊢ Pinv ∗ e'                := sep_mono_left intuitionisticallyIf_elim
-    _ ⊢ Pinv ∗ e' ∗ emp          := sep_mono_right sep_emp.mpr
-    _ ⊢ Pinv ∗ e' ∗ (Pin -∗ Pin) := sep_mono_right <| sep_mono_right wand_rfl
-    _ ⊢ Pinv ∗ e'' ∗ Pin         := sep_mono_right pfPin
-    _ ⊢ Pinv ∗ Pin ∗ e''         := sep_mono_right sep_comm.mp
-  cases mPclose with simp_all
-  | none => calc
-    e ⊢ Pinv ∗ Pin ∗ e''                       := h1
-    _ ⊢ Pinv ∗ Pin ∗ ∀ x, Pout x -∗ Q' x       :=
-        sep_mono_right <| sep_mono_right <| forall_intro hAcc
-    _ ⊢ Pinv ∗ Pin ∗ ∀ x, Pout x ∗ emp -∗ Q' x :=
-        sep_mono_right <| sep_mono_right <| forall_mono <| fun _ => wand_mono_left sep_emp.mp
-    _ ⊢ goal                                   := h0
-  | some mPclose => calc
-    e ⊢ Pinv ∗ Pin ∗ e''                              := h1
-    _ ⊢ Pinv ∗ Pin ∗ ∀ x, Pout x -∗ mPclose x -∗ Q' x :=
-        sep_mono_right <| sep_mono_right <| forall_intro hAcc
-    _ ⊢ Pinv ∗ Pin ∗ ∀ x, Pout x ∗ mPclose x -∗ Q' x  :=
-        sep_mono_right <| sep_mono_right <| forall_intro (forall_elim · |>.trans wand_curry.mp)
-    _ ⊢ goal                                          := h0
+    e ⊢ goal := sorry
 
-private def iInvCore {u} {prop : Q(Type u)} {bi e} (hyps : Hyps bi e) (goal : Q($prop))
-    (ivar : IVarId)
-    (specPat : Option SpecPat)
-    (introPat : Syntax × IntroPat)
-    (closePat : Option <| Syntax × IntroPat) :
+  -- have h0 := inst.elim_inv hϕ
+  -- have h1 : e ⊢ Pinv ∗ Pin ∗ ∀ a, Pout a -∗ Option.map (fun x => x a) mPclose -∗? Q' a := calc
+  --   e ⊢ e' ∗ □?p Pinv            := pf.mp
+  --   _ ⊢ □?p Pinv ∗ e'            := sep_comm.mp
+  --   _ ⊢ Pinv ∗ e'                := sep_mono_left intuitionisticallyIf_elim
+  --   _ ⊢ Pinv ∗ e' ∗ emp          := sep_mono_right sep_emp.mpr
+  --   _ ⊢ Pinv ∗ e' ∗ (Pin -∗ Pin) := sep_mono_right <| sep_mono_right wand_rfl
+  --   _ ⊢ Pinv ∗ e'' ∗ Pin         := sep_mono_right pfPin
+  --   _ ⊢ Pinv ∗ Pin ∗ e''         := sep_mono_right sep_comm.mp
+  --   _ ⊢ _                        := sep_mono_right <| sep_mono_right <| forall_intro (wand_intro <| hAcc ·)
+  -- apply h1.trans
+  -- cases mPclose with
+  -- | none =>
+  --   apply (sep_mono_right <| sep_mono_right <| forall_mono <| fun _ => wand_mono_left sep_emp.mp).trans h0
+  -- | some _ =>
+  --   apply (sep_mono_right <| sep_mono_right <| forall_intro (forall_elim · |>.trans wand_curry.mp)).trans h0
+
+private def iInvCore {u} {prop : Q(Type u)} {bi} {e}
+    (hyps : Hyps bi e) (goal : Q($prop)) (ivar : IVarId) (specPat : Option SpecPat)
+    (introPat : iCasesPat) (closePat : Option iCasesPat) :
     ProofModeM Q($e ⊢ $goal) := do
   -- Find the hypothesis from the context
   let ⟨_, hyps', _, Pinv, _, _ , pfEq⟩ := hyps.remove false ivar
@@ -80,25 +70,31 @@ private def iInvCore {u} {prop : Q(Type u)} {bi e} (hyps : Hyps bi e) (goal : Q(
   have : $out'' =Q $Pin := ⟨⟩
   have : $p'' =Q false := ⟨⟩
 
-  -- Create the wand proposition and apply the introduction pattern to destruct the premise
-  let hAcc : Q(∀ x : $X, $e'' ⊢ $Pout x -∗ optionMap $mPclose x -∗? $Q' x) ←
-    withLocalDeclDQ (u := 0) (← mkFreshUserName `x) X fun x => do
-      let body ← match mPclose with
-      | ~q(none) =>
-        iIntroCore hyps'' q(iprop($Pout $x -∗ $Q' $x)) [introPat]
-      | ~q(some $f) =>
-        match closePat with
-        | some closePat =>
-          let f : Q($X → $prop) := f
-          iIntroCore hyps'' q(iprop($Pout $x -∗ $f $x -∗ $Q' $x)) [introPat, closePat]
-        -- Throw an error if `hclose` is not given, but `mPclose` is not `none`
-        | none => throwError "iinv: error"
-      mkLambdaFVars #[x] body
-
   -- Solve side conditions automatically if possible, otherwise add them into the proof state
   let hϕ ← iSolveSidecondition q($ϕ) false
 
-  return q(tac_inv_elim $inst $hϕ $hAcc $pfEq $pfPin)
+  match mPclose with
+  | ~q(some $f) =>
+
+    let hAcc : Q(∀ x : $X, $e'' ∗ $Pout x ∗ $f x ⊢ $Q' x) ←
+      withLocalDeclDQ (u := 0) (← mkFreshUserName `x) X fun x => do
+        match closePat with
+        | some closePat =>
+          mkLambdaFVars #[x] <| ← iCasesCore _ hyps'' q(iprop($Q' $x)) (.conjunction [introPat, closePat]) q(false) q(iprop($Pout $x ∗ $f $x))
+        -- Throw an error if `hclose` is not given, but `mPclose` is not `none`
+        | none => throwError "iinv: error"
+
+    return q(tac_inv_elim $inst $hϕ $hAcc $pfEq $pfPin)
+
+  | ~q(none) =>
+    let hAcc : Q(∀ x : $X, $e'' ∗ $Pout x ⊢ $Q' x) ←
+      withLocalDeclDQ (u := 0) (← mkFreshUserName `x) X fun x => do
+        mkLambdaFVars #[x] <| ← iCasesCore _ hyps'' q(iprop($Q' $x)) introPat q(false) q($Pout $x)
+
+    let hAcc : Q(∀ x : $X, $e'' ∗ $Pout x ∗ emp ⊢ $Q' x) :=
+      q(fun x => sep_assoc.mpr.trans <| sep_emp.mp.trans ($hAcc x))
+
+    return q(tac_inv_elim $inst $hϕ $hAcc $pfEq $pfPin)
 
 /-- Find an invariant hypothesis with a given `Namespace` value. -/
 private def findInvariantWithNamespace {u} {prop : Q(Type u)} {bi : Q(BI $prop)} {e}
@@ -116,14 +112,14 @@ private def findInvariantWithNamespace {u} {prop : Q(Type u)} {bi : Q(BI $prop)}
 
 /-- `iinv` opens an invariant in the proof state. -/
 syntax (name := iinv) "iinv " colGt term (" $$ " colGt ppSpace specPat)?
-    " with " colGt introPat (colGt introPat)? : tactic
+    " with " colGt icasesPat (colGt icasesPat)? : tactic
 
 elab_rules : tactic
-  | `(tactic| iinv $t:term $[$$ $spat:specPat]? with $ipat:introPat $[$cpat:introPat]?) => do
+  | `(tactic| iinv $t:term $[$$ $spat:specPat]? with $ipat:icasesPat $[$cpat:icasesPat]?) => do
     -- Parse the introduction and selection patterns
     let specPat ← liftMacroM <| spat.mapM SpecPat.parse
-    let introPat ← liftMacroM <| IntroPat.parse ipat
-    let closePat ← liftMacroM <| cpat.mapM IntroPat.parse
+    let introPat ← liftMacroM <| iCasesPat.parse ipat
+    let closePat ← liftMacroM <| cpat.mapM iCasesPat.parse
 
     ProofModeM.runTactic λ mvar { hyps, goal, .. } => do
       -- Find the invariant hypothesis
