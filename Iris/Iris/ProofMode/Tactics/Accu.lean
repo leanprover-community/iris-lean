daf3bb5743ee3cf82d8ab9f1d7b5c7490ee0e59a
Alvin Tang <alvin.tang@ist.ac.at>
2026-06-28 13:53:59 +0200

    Implement `iaccu`

---
+++ Iris/Iris/ProofMode/Tactics/Accu.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2026 Alvin Tang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michael Sammler, Alvin Tang
+-/
+module
+
+public meta import Iris.ProofMode.ProofModeM
+
+namespace Iris.ProofMode
+
+public meta section
+open Lean Elab Tactic Meta Qq
+
+elab "iaccu" : tactic => do
+  ProofModeM.runTactic λ mvar { hyps, goal, .. } => do
+    if !goal.isMVar then
+      throwError m!"iaccu: {goal} is not a metavariable"
+    let ⟨_, pf⟩ := hyps.buildAccuProof
+    mvar.assign pf
