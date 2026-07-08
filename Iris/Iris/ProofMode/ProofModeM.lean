b5225ff4a80a35c9516356a447ededddd63f7dd0
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-08 17:18:13 +0200

    Minor update

--- Iris/Iris/ProofMode/ProofModeM.lean
+++ Iris/Iris/ProofMode/ProofModeM.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Michael Sammler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Michael Sammler, Zongyuan Liu, Yunsong Yang
+Authors: Michael Sammler, Zongyuan Liu, Yunsong Yang, Alvin Tang
 -/
 module
 
