05ee95796f524d086b063ca50a6882af64e109e1
ayhon <ayhonnick@gmail.com>
2026-07-18 12:40:46 +0200

    fix: typo

--- Iris/Iris/BI/WeakestPre.lean
+++ Iris/Iris/BI/WeakestPre.lean
@@ -40,7 +40,7 @@ instance : Std.IsPreorder Stuckness where
 @[simp] theorem le_MaybeStuck {s : Stuckness} : s ≤ MaybeStuck := by
   cases s <;> grind only [Stuckness, LE.le, instLE]
 
-@[simp] theorem NotSuck_le {s : Stuckness} : NotStuck ≤ s := by
+@[simp] theorem NotStuck_le {s : Stuckness} : NotStuck ≤ s := by
   cases s <;> grind only [Stuckness, LE.le, instLE]
 
 end Stuckness
