01c614144d898fa291aba5751ecd0223acb2241b
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-21 17:52:24 +0200

    Another `SIdx` instance

--- IrisMath/IrisMath/StepIndex.lean
+++ IrisMath/IrisMath/StepIndex.lean
@@ -14,24 +14,55 @@ noncomputable section
 
 open Iris
 
-instance instSIdxOrdinal : SIdx Ordinal.{u} where
+instance : SIdx Ordinal.{u} where
   toLT   := inferInstance
   toLE   := inferInstance
   toZero := inferInstance
   succ := Order.succ
-  lt_trans h₁ h₂ := _root_.lt_trans h₁ h₂
+  lt_trans := lt_trans
   lt_wf := Ordinal.lt_wf
   lt_trichotomyT n m :=
     if h : n < m then .inl h
     else if h' : m < n then .inr <| .inr h'
     else .inr <| .inl <| le_antisymm (not_lt.mp h') (not_lt.mp h)
   le_lteq := le_iff_lt_or_eq
-  not_lt_zero n := by simp
-  lt_succ_self n := Order.lt_succ n
-  succ_le_of_lt h := Order.succ_le_of_lt h
+  not_lt_zero _ := by simp
+  lt_succ_self := Order.lt_succ
+  succ_le_of_lt := Order.succ_le_of_lt
   weak_case n :=
     letI : Decidable (∃ m, n = Order.succ m) := Classical.propDecidable _
     if h : ∃ m, n = Order.succ m then
       .inl ⟨h.choose, h.choose_spec⟩
     else
       .inr fun m hm => lt_of_le_of_ne (Order.succ_le_of_lt hm) fun he => h ⟨m, he.symm⟩
+
+@[reducible]
+noncomputable def ordinalSIdx (κ : Ordinal.{u}) (hκ : Order.IsSuccLimit κ) :
+    SIdx {o : Ordinal.{u} // o < κ} where
+  toLT   := inferInstance
+  toLE   := inferInstance
+  toZero := by constructor; constructor; exact hκ.pos
+  succ := fun o => ⟨Order.succ o.1, hκ.succ_lt o.2⟩
+  lt_trans h₁ h₂ := _root_.lt_trans h₁ h₂
+  lt_wf := InvImage.wf Subtype.val Ordinal.lt_wf
+  lt_trichotomyT n m :=
+    if h : n < m then .inl h
+    else if h' : m < n then .inr <| .inr h'
+    else .inr <| .inl <| le_antisymm (not_lt.mp h') (not_lt.mp h)
+  le_lteq := le_iff_lt_or_eq
+  not_lt_zero n :=
+    show ¬n.val < 0
+    by simp
+  lt_succ_self n := Order.lt_succ n.val
+  succ_le_of_lt {n m} h := by
+    change Order.succ n.val ≤ m.val
+    simpa
+  weak_case n :=
+    letI : Decidable (∃ m, n = (fun o : { o // o < κ } => ⟨Order.succ o.1, hκ.succ_lt o.2⟩) m) :=
+      Classical.propDecidable _
+    if h : ∃ m, n = (fun o => ⟨Order.succ o.1, hκ.succ_lt o.2⟩) m then
+      .inl ⟨h.choose, h.choose_spec⟩
+    else
+      .inr fun m hm =>
+        lt_of_le_of_ne (Order.succ_le_of_lt hm : Order.succ m.val ≤ n.val)
+          fun he => h ⟨m, he.symm⟩
