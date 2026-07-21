1902ba566c281786f3d244aa6e41278cfcfe59f0
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-21 11:00:02 +0200

    Port `AsEmpValid0` and `as_emp_valid_embed`

--- Iris/Iris/ProofMode/Classes.lean
+++ Iris/Iris/ProofMode/Classes.lean
@@ -38,6 +38,11 @@ theorem asEmpValid_2 {PROP} [bi : BI PROP] {P: PROP} {io : InOut}
     (φ : Prop) (inst : AsEmpValid .from φ io PROP .out bi P) : (⊢ P) → φ :=
   (AsEmpValid.as_emp_valid io .out).2 rfl
 
+@[ipm_class, rocq_alias AsEmpValid0]
+class AsEmpValid0 (d : AsEmpValid.Direction) (φ : Prop) (_ : InOut) (PROP : semiOutParam $ Type _)
+    (_ : InOut) (bi : semiOutParam $ BI PROP) (P : outParam PROP) where
+  as_emp_valid_0 : AsEmpValid d φ .in PROP .in bi P
+
 /- Depending on the use case, type classes with the prefix `From` or `Into` are used. Type classes
 with the prefix `From` are used to generate one or more propositions *from* which the original
 proposition can be derived. Type classes with the prefix `Into` are used to generate propositions
