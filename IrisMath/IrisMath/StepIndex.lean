7a0fb5c34a34c8faccf2ce0a474a62cad32182c5
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-21 20:35:35 +0200

    Introduce `ordinalToTypeSIdx`

--- IrisMath/IrisMath/StepIndex.lean
+++ IrisMath/IrisMath/StepIndex.lean
@@ -43,7 +43,7 @@ def ordinalSubtypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) :
   toLE := Subtype.instLE
   toZero := by constructor; constructor; exact hκ.pos
   succ o := ⟨Order.succ o.val, hκ.succ_lt o.2⟩
-  lt_trans h₁ h₂ := _root_.lt_trans h₁ h₂
+  lt_trans := lt_trans
   lt_wf := InvImage.wf Subtype.val Ordinal.lt_wf
   lt_trichotomyT n m :=
     if h : n < m then .inl h
@@ -67,6 +67,38 @@ def ordinalSubtypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) :
       .inr fun m hm =>
         lt_of_le_of_ne (Order.succ_le_of_lt hm : Order.succ m.val ≤ n.val)
           fun he => h ⟨m, he.symm⟩
+
+@[reducible]
+def ordinalToTypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) :
+    SIdx (Ordinal.ToType κ) :=
+  haveI : Nonempty κ.ToType := Ordinal.nonempty_toType_iff.mpr hκ.pos.ne'
+  letI : OrderBot κ.ToType := WellFoundedLT.toOrderBot κ.ToType
+  haveI : NoMaxOrder κ.ToType := by
+    apply Ordinal.isSuccPrelimit_type_lt_iff.mp
+    simp only [Ordinal.type_toType, hκ.isSuccPrelimit]
+  {
+    toLT := instDistribLatticeOfLinearOrder.toSemilatticeInf.toLT
+    toLE := Preorder.toLE
+    toZero := ⟨⊥⟩
+    succ := Order.succ
+    lt_trans := lt_trans
+    lt_wf := wellFounded_lt
+    lt_trichotomyT n m :=
+      if h : n < m then .inl h
+      else if h' : m < n then .inr <| .inr h'
+      else .inr <| .inl <| le_antisymm (not_lt.mp h') (not_lt.mp h)
+    le_lteq := le_iff_lt_or_eq
+    not_lt_zero _ := not_lt_bot
+    lt_succ_self := Order.lt_succ
+    succ_le_of_lt := Order.succ_le_of_lt
+    weak_case n :=
+      letI : Decidable (∃ m, n = Order.succ m) := Classical.propDecidable _
+      if h : ∃ m, n = Order.succ m then
+        .inl ⟨h.choose, h.choose_spec⟩
+      else
+        .inr fun m hm =>
+          lt_of_le_of_ne (Order.succ_le_of_lt hm) fun he => h ⟨m, he.symm⟩
+  }
 
 theorem limit_iff_isSuccLimit {o : Ordinal} : SIdx.Limit o ↔ Order.IsSuccLimit o := by
   constructor
