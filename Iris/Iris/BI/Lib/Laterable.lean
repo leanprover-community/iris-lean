df499242da4d2ea5ab5a029a85c5aa9f5d52ab8b
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-24 16:39:51 +0200

    Ignore `make_laterable_mono'` and `make_laterable_proper`

--- Iris/Iris/BI/Lib/Laterable.lean
+++ Iris/Iris/BI/Lib/Laterable.lean
@@ -100,11 +100,12 @@ instance make_laterable_ne [BI PROP] : NonExpansive <| make_laterable (PROP := P
     apply wand_ne.ne; rfl
     apply except0_ne.ne; assumption
 
-/- TODO: `make_laterable_proper`, `make_laterable_mono'`, `make_laterable_flip_mono'` -/
-
 @[rw_mono_rule, rocq_alias make_laterable_mono]
 theorem make_laterable_mono [BI PROP] {Q1 Q2 : PROP} (h : Q1 ⊢ Q2) :
     make_laterable Q1 ⊢ make_laterable Q2 := by unfold make_laterable; rw' [h]
+
+#rocq_ignore make_laterable_mono' "No Proper type class in Lean"
+#rocq_ignore make_laterable_proper "No Proper type class in Lean"
 
 @[rocq_alias make_laterable_except_0]
 theorem make_laterable_except_0 [BI PROP] {Q : PROP} :
