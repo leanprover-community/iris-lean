4a0eb10f8c34b1298490f5902a3b7e48b77d097a
Oliver Soeser <hello@oliversoeser.com>
2026-07-26 15:04:09 +0200

    twp.pre_mono manual proof port

--- Iris/Iris/ProgramLogic/TotalWeakestPre.lean
+++ Iris/Iris/ProgramLogic/TotalWeakestPre.lean
@@ -5,7 +5,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 module
 
 public import Iris.ProgramLogic.WeakestPre
-public import Iris.BI.Lib.MonotonePred
 
 namespace Iris
 
@@ -35,6 +34,28 @@ def twp.pre (s : Stuckness) (wp : CoPset -> Expr -> (Val -> IProp GF) -> IProp G
       ⌜obs = []⌝ ∗ stateInterp σ₂ (ns + 1) obs' (nt + eₜ.length) ∗
       wp E e₂ Φ ∗ [∗list] e' ∈ eₜ, wp ⊤ e' ι.forkPost)
 
+@[rocq_alias twp_pre_mono]
+theorem twp.pre_mono (s : Stuckness) (wp₁ wp₂ : CoPset -> Expr -> (Val -> IProp GF) -> IProp GF) :
+    ⊢ (□ ∀ E e Φ, wp₁ E e Φ -∗ wp₂ E e Φ) →
+      ∀ E e Φ, twp.pre s wp₁ E e Φ -∗ twp.pre s wp₂ E e Φ := by
+  iintro #H %E %e %Φ Hwp
+  unfold pre
+  split
+  · iassumption
+  · iintro %σ₁ %ns %obs' %nt Hσ
+    imod Hwp $$ Hσ with ⟨$, Hwp⟩
+    imodintro
+    iintro %obs %e₂ %σ₂ %et Hstep
+    imod Hwp $$ Hstep with ⟨$, Hσ, Hwp, Hfork⟩
+    imodintro
+    iframe Hσ
+    isplitl [Hwp]
+    · iapply H $$ Hwp
+    · iapply BI.BigSepL.bigSepL_impl $$ Hfork
+      iintro !> %k %e %_ Hwp
+      iapply H $$ Hwp
+
 open Function in
+@[rocq_alias twp_pre']
 def twp.pre' (s : Stuckness) (wp : (CoPset × Expr) × (Val -> IProp GF) -> IProp GF) :=
     uncurry <| uncurry <| @twp.pre hlc Expr State Obs Val Λ GF ι s (curry <| curry wp)
