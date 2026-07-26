6fd5e3b71a4e55fe232371ecfd935c12f38d5b9e
Alvin Tang <alvin.tang@ist.ac.at>
2026-07-26 21:09:22 +0200

    Port `impl_wand` and `impl_wand_2`

--- Iris/Iris/BI/DerivedLaws.lean
+++ Iris/Iris/BI/DerivedLaws.lean
@@ -2222,6 +2222,15 @@ theorem persistent_and_sep [BI PROP] [BIAffine PROP] {P Q : PROP} :
   | TCOr.r => (and_congr_right persistently_iff.symm).trans <|
               and_persistently_iff_sep.trans (sep_congr_right persistently_iff)
 
+
+@[rocq_alias bi.impl_wand_2]
+theorem imp_wand_2 [BI PROP] {P Q : PROP} [Persistent P] :
+    (P -∗ Q) ⊢ P → Q := imp_intro <| persistent_and_sep_mp.trans wand_elim_left
+
+@[rocq_alias bi.impl_wand]
+theorem imp_wand [BI PROP] [BIAffine PROP] {P Q : PROP} [Persistent P] :
+    (P → Q) ⊣⊢ (P -∗ Q) := ⟨imp_wand_1, imp_wand_2⟩
+
 @[rocq_alias bi.persistent_sep_dup_1]
 theorem persistent_sep_dup_mp [BI PROP] {P : PROP} [inst : Persistent P] : P ⊢ P ∗ P :=
   and_self.mpr.trans persistent_and_sep_mp
