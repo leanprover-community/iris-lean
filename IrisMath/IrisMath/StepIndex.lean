9b4ee5fa724e601af5844c36292e75787c615add
Sergei Stepanenko <kaptch@gmail.com>
2026-07-27 02:37:22 +0200

    fix: rm overlapping instance (#541)

--- IrisMath/IrisMath/StepIndex.lean
+++ IrisMath/IrisMath/StepIndex.lean
@@ -15,9 +15,9 @@ noncomputable section
 open Iris
 
 instance ordinalSIdx : SIdx Ordinal where
-  toLT := Ordinal.partialOrder.toLT
-  toLE := Preorder.toLE
-  toZero := Ordinal.zero
+  toLT := inferInstance
+  toLE := inferInstance
+  toZero := inferInstance
   succ := Order.succ
   lt_trans := lt_trans
   lt_wf := Ordinal.lt_wf
@@ -40,40 +40,6 @@ instance ordinalSIdx : SIdx Ordinal where
         exact h ⟨m, he.symm⟩
 
 @[reducible]
-def ordinalSubtypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) :
-    SIdx {o : Ordinal // o < κ} where
-  toLT := Subtype.instLT
-  toLE := Subtype.instLE
-  toZero := by constructor; constructor; exact hκ.pos
-  succ o := ⟨Order.succ o.val, hκ.succ_lt o.2⟩
-  lt_trans := lt_trans
-  lt_wf := InvImage.wf Subtype.val Ordinal.lt_wf
-  lt_trichotomyT n m :=
-    if h : n < m then by left; exact h
-    else if h' : m < n then by right; right; exact h'
-    else by right; left; exact le_antisymm (not_lt.mp h') (not_lt.mp h)
-  le_lteq := le_iff_lt_or_eq
-  not_lt_zero n :=
-    show ¬n.val < 0
-    by simp
-  lt_succ_self n := Order.lt_succ n.val
-  succ_le_of_lt {n m} h := by
-    change Order.succ n.val ≤ m.val
-    simpa
-  weak_case n :=
-    let succFn : {o : Ordinal // o < κ} → {o : Ordinal // o < κ} :=
-      fun o => ⟨Order.succ o.val, hκ.succ_lt o.property⟩
-    letI : Decidable (∃ m, n = succFn m) := Classical.propDecidable _
-    if h : ∃ m, n = succFn m then by
-      left; exact ⟨h.choose, h.choose_spec⟩
-    else by
-      right; intro m hm
-      apply lt_of_le_of_ne
-      · exact (Order.succ_le_of_lt hm : Order.succ m.val ≤ n.val)
-      · intro he
-        exact h ⟨m, he.symm⟩
-
-@[reducible]
 def ordinalToTypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) : SIdx κ.ToType :=
   haveI : Nonempty κ.ToType := Ordinal.nonempty_toType_iff.mpr hκ.pos.ne'
   letI : OrderBot κ.ToType := WellFoundedLT.toOrderBot κ.ToType
@@ -81,8 +47,8 @@ def ordinalToTypeSIdx (κ : Ordinal) (hκ : Order.IsSuccLimit κ) : SIdx κ.ToTy
     apply Ordinal.isSuccPrelimit_type_lt_iff.mp
     simp only [Ordinal.type_toType, hκ.isSuccPrelimit]
   {
-    toLT := instDistribLatticeOfLinearOrder.toSemilatticeInf.toLT
-    toLE := Preorder.toLE
+    toLT := inferInstance
+    toLE := inferInstance
     toZero := ⟨⊥⟩
     succ := Order.succ
     lt_trans := lt_trans
