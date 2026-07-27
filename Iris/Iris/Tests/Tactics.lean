2663e20af24b9a08566cf3bdf6ec756519f1b2ef
Oliver Soeser <hello@oliversoeser.com>
2026-07-27 16:01:45 +0200

    fix irewrite tests

--- Iris/Iris/Tests/Tactics.lean
+++ Iris/Iris/Tests/Tactics.lean
@@ -2297,8 +2297,7 @@ example (P Q : PROP) :
   iintro #HPQ HQ !>
   inext
   irewrite [HPQ] at HQ
-  · exact ⟨fun _ _ _ h => affinely_ne.ne h⟩
-  · iexact HQ
+  iexact HQ
 
 /- Tests `irewrite` under affine and later backwards -/
 example (P Q : PROP) :
@@ -2306,8 +2305,7 @@ example (P Q : PROP) :
   iintro #HPQ HQ !>
   inext
   irewrite [←HPQ] at HQ
-  · exact ⟨fun _ _ _ h => affinely_ne.ne h⟩
-  · iexact HQ
+  iexact HQ
 
 /- Tests `irewrite` with no matching target -/
 /--
