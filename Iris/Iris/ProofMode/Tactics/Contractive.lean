372f34f3f2e2cc89329040d38d4c509b5c5e2635
Oliver Soeser <hello@oliversoeser.com>
2026-07-29 15:51:57 +0200

    remove debug

--- Iris/Iris/ProofMode/Tactics/Contractive.lean
+++ Iris/Iris/ProofMode/Tactics/Contractive.lean
@@ -35,7 +35,7 @@ meta def distInstanceStep : TacticM Bool := do
   let tac ← `(tactic|apply $(mkIdent ``OFE.Contractive.distLater_dist); intro _ _)
   return ← observingSuccess <| evalTactic tac
 
-meta def distHypStep : TacticM Bool := do
+meta def distLaterStep : TacticM Bool := do
   let goal ← getMainGoal
   pure <| ← goal.withContext do
     let ctx ← getLCtx
@@ -44,6 +44,19 @@ meta def distHypStep : TacticM Bool := do
         if decl.type.isAppOf ``OFE.DistLater then
           let declIdent := mkIdent decl.userName
           let tac ← `(tactic|apply $declIdent:ident; assumption)
+          if ← observingSuccess <| evalTactic tac then
+            return true
+    return false
+
+meta def distStep : TacticM Bool := do
+  let goal ← getMainGoal
+  pure <| ← goal.withContext do
+    let ctx ← getLCtx
+    for decl? in ctx.decls do
+      if let some decl := decl? then
+        if decl.type.isAppOf ``OFE.Dist then
+          let declIdent := mkIdent decl.userName
+          let tac ← `(tactic|apply $declIdent:ident)
           if ← observingSuccess <| evalTactic tac then
             return true
     return false
@@ -60,26 +73,28 @@ meta def makeMainGoal (goal : MVarId) : TacticM Unit := do
   let goals := goal :: goals.erase goal
   setGoals goals
 
-meta def recurseTactic (t : MVarId → Bool → TacticM Unit) (guarded : Bool) : TacticM Unit := do
-  let _ ← (← getUnsolvedGoals).mapM (t · guarded)
+mutual
+
+meta partial def contractiveRecurse (guarded : Bool) : TacticM Unit := do
+  let _ ← (← getUnsolvedGoals).mapM (contractiveMain · guarded)
 
 meta partial def contractiveMain (goal : MVarId) (guarded : Bool) : TacticM Unit := do
   if ← goal.isAssigned then return
   makeMainGoal goal
 
   -- simplification step (includes application of Dist.rfl)
-  if let some _ ← observing? (evalTactic <| ← `(tactic|simp)) then
-    recurseTactic contractiveMain guarded; return
+  if let some _ ← observing? (evalTactic <| ← `(tactic|simp)) then contractiveRecurse guarded; return
 
   -- uses an OFE.Contractive instance
-  if not guarded then if ← distInstanceStep then
-    recurseTactic contractiveMain true; return
+  if not guarded then if ← distInstanceStep then contractiveRecurse true; return
 
   -- applies an OFE.DistLater hypothesis
-  if ← distHypStep then recurseTactic contractiveMain guarded; return
+  if ← distLaterStep then contractiveRecurse guarded; return
 
   -- applies a non-expansive lemma
-  if ← nonexpStep then recurseTactic contractiveMain guarded; return
+  if ← nonexpStep then contractiveRecurse guarded; return
+
+end
 
 elab "contractive" : tactic => do
   -- intro hypotheses
@@ -94,3 +109,37 @@ elab "contractive" : tactic => do
 
   -- main loop
   contractiveMain (← getMainGoal) false
+
+mutual
+
+meta partial def nonexpRecurse : TacticM Unit := do
+  let _ ← (← getUnsolvedGoals).mapM (nonexpMain ·)
+
+meta partial def nonexpMain (goal : MVarId) : TacticM Unit := do
+  if ← goal.isAssigned then return
+  makeMainGoal goal
+
+  -- simplification step (includes application of Dist.rfl)
+  if let some _ ← observing? (evalTactic <| ← `(tactic|simp)) then nonexpRecurse; return
+
+  -- applies an OFE.Dist hypothesis
+  if ← distStep then nonexpRecurse; return
+
+  -- applies a non-expansive lemma
+  if ← nonexpStep then nonexpRecurse; return
+
+end
+
+elab "nonexp" : tactic => do
+  -- intro hypotheses
+  evalTactic <| ← `(tactic|intros)
+
+  -- intro foralls within OFE.Dist
+  while ← distIsForall <| ← getMainTarget do
+   evalTactic <| ← `(tactic|intro)
+
+  -- unfold function definition
+  tryUnfoldFn
+
+  -- main loop
+  nonexpMain (← getMainGoal)
