56ee71152433839fe3e645b74634a23a6bab3fd6
Michael Sammler <noreply@sammler.me>
2026-07-31 13:01:43 +0200

    add heaplang to rocq_ignore

--- Iris/Iris/HeapLang/Lib/NondetBool.lean
+++ Iris/Iris/HeapLang/Lib/NondetBool.lean
@@ -11,7 +11,7 @@ public section
 
 variable [HeapLangGS hlc GF]
 
-@[rocq_alias nondet_bool]
+@[rocq_alias heap_lang.nondet_bool]
 def nondetBool := hl_val% λ _,
     let l := ref(#true);
     fork(
@@ -19,7 +19,7 @@ def nondetBool := hl_val% λ _,
     );
     !l
 
-@[rocq_alias nondet_bool_spec]
+@[rocq_alias heap_lang.nondet_bool_spec]
 theorem nondetBool.spec : ⊢@{IProp GF}
     {{ True }} hl(&nondetBool #()) {{ (b : Bool), RET hl_val(#b); True }} := by
   iintro %Φ - K
