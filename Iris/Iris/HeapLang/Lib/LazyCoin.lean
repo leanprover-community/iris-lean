c05d224f0627953ee412ee64ad6b09a41219df3e
ayhon <ayhonnick@gmail.com>
2026-07-31 12:05:14 +0200

    feat: add rocq_alias annotations

--- Iris/Iris/HeapLang/Lib/LazyCoin.lean
+++ Iris/Iris/HeapLang/Lib/LazyCoin.lean
@@ -7,10 +7,12 @@ public import Iris.HeapLang.Lib.NondetBool
 namespace Iris.HeapLang
 
 -- type Coin := Ref (Option Bool) × ProphId
+@[rocq_alias new_coin]
 def newCoin := hl_val%
   λ _, (ref(none()), newProph())
 
 -- readCoin(cp : Coin) : Bool
+@[rocq_alias read_coin]
 def readCoin := hl_val%
   λ cp,
     let c := fst(cp);
@@ -30,23 +32,28 @@ section Proofs
 
 open HeapLang
 
+#rocq_ignore val_to_bool "prohecy_to_bool no longer makes use of this definition"
+
+@[rocq_alias prophecy_to_bool]
 def prophecyToBool (vs : List (Val × Val)) : Bool :=
   if let some (_, v) := vs.head? then
     v = hl_val(#true)
   else false
 
-@[simp, grind =]
+@[simp, grind =, rocq_alias prophecy_to_bool_of_bool]
 theorem prophecyToBool_of_bool (vs : List (Val × Val)) (v : Val) (b : Bool) :
     prophecyToBool ((v, hl_val(#b)) :: vs) = b := by
   cases b <;> rfl
 
 /-- `cp` is a pair of `c` and `p`, where if p is a prophecy with resolutions `vs`
     such that if `c` is non-null, it points to the first resolution value of  `vs` -/
+@[rocq_alias coin]
 def coin (cp : Val) (b : Bool) : IProp GF := iprop%
   ∃ (c : Loc) (p : ProphId) (vs : List (Val × Val)),
   ⌜ cp = hl_val((#c, #p)) ⌝ ∗ proph p vs ∗
   (c ↦ hl_val(some(#b)) ∨ (c ↦ hl_val(none()) ∗ ⌜ b = prophecyToBool vs ⌝))
 
+@[rocq_alias new_coin_spec]
 theorem newCoin.spec : ⊢@{IProp GF}
     {{ True }} hl(&newCoin #()) {{ c b, RET c; coin c b }} := by
   iintro %Φ - K
@@ -68,6 +75,7 @@ theorem newCoin.spec : ⊢@{IProp GF}
   ipureintro
   rfl
 
+@[rocq_alias read_coin_spec]
 theorem readCoin.spec (cp : Val) (b : Bool) : ⊢@{IProp GF}
     {{ coin cp b }} hl(&readCoin &cp) {{ RET hl_val(#b); coin cp b }} := by
   conv => enter [1,1]; unfold coin
