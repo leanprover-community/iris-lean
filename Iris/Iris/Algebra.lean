81b55cc673774e236bc13c9aac02209f9cdd8c36
Alvin Tang <alvin.tang@ist.ac.at>
2026-08-09 14:52:26 +0200

    Add missing imports, avoid circular dependency between `BI` and `ProofMode`

--- Iris/Iris/Algebra.lean
+++ Iris/Iris/Algebra.lean
@@ -19,6 +19,7 @@ public import Iris.Algebra.IsOp
 public import Iris.Algebra.LeibnizSet
 public import Iris.Algebra.List
 public import Iris.Algebra.LocalUpdates
+public import Iris.Algebra.Numbers
 public import Iris.Algebra.OFE
 public import Iris.Algebra.ReservationMap
 public import Iris.Algebra.StepIndex
