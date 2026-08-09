e2640d23ff0c90c085f358180d667f03f22602b4
Alvin Tang <alvin.tang@ist.ac.at>
2026-08-09 16:42:25 +0200

    Add Mathlib style linters into Iris/Std
    With modifications to:
    * remove Mathlib-specific content
    * rename the linter options (`linter.style.*` -> `linter.iris.style.*`)

--- Iris/Iris/Std/Linters.lean
+++ Iris/Iris/Std/Linters.lean
@@ -4,5 +4,5 @@ Released under Apache 2.0 license as described in the file LICENSE.
 -/
 module
 
-public meta import Iris.Std.RocqPorting
-public meta import Iris.Std.Linters
+public import Iris.Std.Linters.DeclarationNames
+public import Iris.Std.Linters.Style
