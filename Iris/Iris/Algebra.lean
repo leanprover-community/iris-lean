838509b29725aafe181cc221a4c9a686fc3c1e7e
Sergei Stepanenko <sergei.stepanenko@cs.au.dk>
2026-08-11 22:55:28 +0200

    init

--- Iris/Iris/Algebra.lean
+++ Iris/Iris/Algebra.lean
@@ -3,6 +3,7 @@ module
 public import Iris.Algebra.Agree
 public import Iris.Algebra.Auth
 public import Iris.Algebra.BigOp
+public import Iris.Algebra.Chain
 public import Iris.Algebra.CMRA
 public import Iris.Algebra.COFESolver
 public import Iris.Algebra.Csum
