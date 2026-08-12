796f38f7dc99bd10acb8ad5807c15c895d85aa40
Alvin Tang <alvin.tang@ist.ac.at>
2026-08-12 20:03:16 +0200

    Remove outdated imports

--- Iris/Iris/Std.lean
+++ Iris/Iris/Std.lean
@@ -5,7 +5,6 @@ public import Iris.Std.BitOp
 public import Iris.Std.Classes
 public import Iris.Std.CoPset
 public import Iris.Std.DelabRule
-public import Iris.Std.DumpPortingData
 public import Iris.Std.Equivalence
 public import Iris.Std.Expr
 public import Iris.Std.FromMathlib
