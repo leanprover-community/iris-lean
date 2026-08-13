52a75533fc0d0f5d985f43e0460c2ee390eb85bf
Markus de Medeiros <markusdemedeiros@outlook.com>
2026-08-13 06:10:15 -0400

    fix numbers

--- IrisMath/IrisMath/Numbers.lean
+++ IrisMath/IrisMath/Numbers.lean
@@ -12,6 +12,7 @@ public import Iris
 -/
 
 @[expose] public section
+local stepindex Nat
 
 /-- Relationship between Mathlib's AddZeroClass to the Stdlib Std.LawfulLeftIdentity on Add. -/
 instance AddZeroClass.to_isLawfulLeftIdentity {M : Type _} [AddZeroClass M] :
