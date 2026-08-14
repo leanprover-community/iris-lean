f6b5e899f9a98f6593fb595517262f4b5da99fb9
Markus de Medeiros <40584146+markusdemedeiros@users.noreply.github.com>
2026-08-14 09:19:48 -0400

    feat: ignore sts.v (#635)
    Co-authored-by: Michael Sammler <MackieLoeffel@users.noreply.github.com>

---
+++ Iris/Iris/Algebra/Porting.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2026. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Markus de Medeiros
+-/
+module
+
+import Iris.Init
+
+#rocq_ignore_file algebra "sts.v" "Historical code, nowadays it is preferred to encode the transition system as a CMRA"
