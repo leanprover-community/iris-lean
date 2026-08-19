e7a0a43814c4f1154ca0c8049883ca56c2288b86
Markus de Medeiros <40584146+markusdemedeiros@users.noreply.github.com>
2026-08-19 07:37:07 -0400

    feat: Port OwnP (#653)
    ---------
    
    Co-authored-by: Zongyuan Liu <liuzongyuan0505@gmail.com>

--- Iris/Iris/ProgramLogic.lean
+++ Iris/Iris/ProgramLogic.lean
@@ -9,6 +9,7 @@ public import Iris.ProgramLogic.EctxLanguage
 public import Iris.ProgramLogic.EctxLifting
 public import Iris.ProgramLogic.Language
 public import Iris.ProgramLogic.Lifting
+public import Iris.ProgramLogic.OwnP
 public import Iris.ProgramLogic.ThreadPool
 public import Iris.ProgramLogic.TotalAdequacy
 public import Iris.ProgramLogic.TotalEctxLifting
