fc9b4c7e09bc41c05cfb4b0ca8a308eebc897bfb
Alvin Tang <alvin.tang@ist.ac.at>
2026-08-22 21:52:00 +0200

    More straightforward instances

--- Iris/Iris/ProofMode/MonPred.lean
+++ Iris/Iris/ProofMode/MonPred.lean
@@ -102,3 +102,117 @@ instance fromLater_monPred_at {α} (φ : Prop) io (sel : α) (n : Nat)
     _ ⊢ ▷^[n] (Q.monPred_at i)      := laterN_mono n hm.make_monPred_at.mpr
     _ ⊢ iprop(▷^[n] Q).monPred_at i := (monPred_at_laterN n i Q).mpr
     _ ⊢ P.monPred_at  i              := entails_at.mp (h.from_modal hφ) i
+
+/-! ### IntoPure -/
+
+@[rocq_alias into_pure_monPred_in]
+instance intoPure_monPred_in (i j : I.car) :
+    IntoPure ((MonPred.monPred_in i : MonPred I PROP).monPred_at j) (I.rel.le i j) where
+  into_pure := (monPred_at_in j i).mp
+
+@[rocq_alias into_pure_monPred_at]
+instance intoPure_monPred_at (P : MonPred I PROP) (φ : Prop) (i : I.car) [h : IntoPure P φ] :
+    IntoPure (P.monPred_at i) φ where
+  into_pure := (entails_at.mp h.into_pure i).trans (monPred_at_pure i φ).mp
+
+/-! ### FromPure -/
+
+@[rocq_alias from_pure_monPred_at]
+instance fromPure_monPred_at (a : Bool) (P : MonPred I PROP) ioφ (φ : Prop) (i : I.car)
+    [h : FromPure a P ioφ φ] : FromPure a (P.monPred_at i) ioφ φ where
+  from_pure := (monPred_at_affinely_if i a iprop(⌜φ⌝)).mpr.trans (entails_at.mp h.from_pure i)
+
+@[rocq_alias from_pure_monPred_in]
+instance fromPure_monPred_in ioφ (i j : I.car) :
+    FromPure false ((MonPred.monPred_in i : MonPred I PROP).monPred_at j) ioφ (I.rel.le i j) where
+  from_pure := (monPred_at_in j i).mpr
+
+/-! ### IntoPersistently -/
+
+@[ipm_backtrack, rocq_alias into_persistent_monPred_at]
+instance (priority := high) intoPersistently_monPred_at (p : Bool)
+    (P Q : MonPred I PROP) (𝓠 : PROP) (i : I.car)
+    [h : IntoPersistently p P Q] [hm : MakeMonPredAt i Q 𝓠] :
+    IntoPersistently p (P.monPred_at i) 𝓠 where
+  into_persistently := calc
+    _ ⊢ iprop(<pers>?p P).monPred_at i  := (monPred_at_persistently_if i p P).mpr
+    _ ⊢ iprop(<pers> Q).monPred_at i    := entails_at.mp h.into_persistently i
+    _ ⊢ <pers> (Q.monPred_at i)         := (monPred_at_persistently i Q).mp
+    _ ⊢ <pers> 𝓠                        := persistently_mono hm.make_monPred_at.mp
+
+/-! ### FromAnd -/
+
+@[rocq_alias from_and_monPred_at]
+instance fromAnd_monPred_at (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : FromAnd P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    FromAnd (P.monPred_at i) 𝓠1 𝓠2 where
+  from_and := (and_mono h1.make_monPred_at.mpr h2.make_monPred_at.mpr).trans
+    (entails_at.mp h.from_and i)
+
+/-! ### IntoAnd -/
+
+@[rocq_alias into_and_monPred_at]
+instance intoAnd_monPred_at (p : Bool) (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : IntoAnd p P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    IntoAnd p (P.monPred_at i) 𝓠1 𝓠2 where
+  into_and := calc
+    _ ⊢ (iprop(□?p P) : MonPred I PROP).monPred_at i          := (monPred_at_intuitionistically_if
+                                                                   i p P).mpr
+    _ ⊢ (iprop(□?p (Q1 ∧ Q2)) : MonPred I PROP).monPred_at i  := entails_at.mp h.into_and i
+    _ ⊢ □?p ((iprop(Q1 ∧ Q2) : MonPred I PROP).monPred_at i)  := (monPred_at_intuitionistically_if
+                                                                   i p iprop(Q1 ∧ Q2)).mp
+    _ ⊢ □?p (𝓠1 ∧ 𝓠2)                                         := intuitionisticallyIf_mono
+        (and_mono h1.make_monPred_at.mp h2.make_monPred_at.mp)
+
+/-! ### FromSep -/
+
+@[rocq_alias from_sep_monPred_at]
+instance fromSep_monPred_at (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : FromSep P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    FromSep (P.monPred_at i) 𝓠1 𝓠2 where
+  from_sep := (sep_mono h1.make_monPred_at.mpr h2.make_monPred_at.mpr).trans
+    (entails_at.mp h.from_sep i)
+
+/-! ### IntoSep -/
+
+@[rocq_alias into_sep_monPred_at]
+instance intoSep_monPred_at (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : IntoSep P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    IntoSep (P.monPred_at i) 𝓠1 𝓠2 where
+  into_sep := (entails_at.mp h.into_sep i).trans
+    (sep_mono h1.make_monPred_at.mp h2.make_monPred_at.mp)
+
+/-! ### FromOr -/
+
+@[rocq_alias from_or_monPred_at]
+instance fromOr_monPred_at (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : FromOr P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    FromOr (P.monPred_at i) 𝓠1 𝓠2 where
+  from_or := (or_mono h1.make_monPred_at.mpr h2.make_monPred_at.mpr).trans
+    (entails_at.mp h.from_or i)
+
+/-! ### IntoOr -/
+
+@[rocq_alias into_or_monPred_at]
+instance intoOr_monPred_at (P Q1 Q2 : MonPred I PROP) (𝓠1 𝓠2 : PROP) (i : I.car)
+    [h : IntoOr P Q1 Q2] [h1 : MakeMonPredAt i Q1 𝓠1] [h2 : MakeMonPredAt i Q2 𝓠2] :
+    IntoOr (P.monPred_at i) 𝓠1 𝓠2 where
+  into_or := (entails_at.mp h.into_or i).trans
+    (or_mono h1.make_monPred_at.mp h2.make_monPred_at.mp)
+
+/-! ### IntoLaterN -/
+
+@[rocq_alias maybe_into_later_monPred_at]
+instance intoLaterN_monPred_at progress (n : Nat) (P Q : MonPred I PROP) (𝓠 : PROP) (i : I.car)
+    [h : IntoLaterN (progress := true) (only_head := false) n P Q]
+    [hm : MakeMonPredAt i Q 𝓠] :
+    IntoLaterN progress (only_head := false) n (P.monPred_at i) 𝓠 where
+  into_laterN := (entails_at.mp h.into_laterN i).trans <|
+    (monPred_at_laterN n i Q).mp.trans (laterN_mono n hm.make_monPred_at.mp)
+
+/-! ### IntoEmbed -/
+
+@[rocq_alias into_embed_objective]
+instance intoEmbed_objective (P : MonPred I PROP) [Objective P] :
+    IntoEmbed (PROP1 := PROP) (PROP2 := MonPred I PROP) P iprop(∀ i, P.monPred_at i) where
+  into_embed := entails_at.mpr fun i => forall_intro fun j => Objective.objective_at i j
