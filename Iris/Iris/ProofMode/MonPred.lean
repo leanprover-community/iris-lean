738845f546bb88d20f30d5dc517d2cba939fb74d
Alvin Tang <alvin.tang@ist.ac.at>
2026-08-22 21:37:06 +0200

    Start porting `ProofMode/MonPred.lean`: `modality_objectively`

---
+++ Iris/Iris/ProofMode/MonPred.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) 2026. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors:
+-/
+module
+
+public import Iris.BI
+public import Iris.BI.MonPred
+public import Iris.ProofMode.Classes
+public import Iris.ProofMode.ClassesMake
+public import Iris.ProofMode.ModalityInstances
+public import Iris.ProofMode.Instances
+public import Iris.Std.TC
+
+@[expose] public section
+
+namespace Iris.ProofMode
+open BI Std MonPred
+
+variable {I : BiIndex} {PROP : Type _} [BI PROP]
+
+@[rocq_alias modality_objectively, rocq_alias modality_objectively_mixin]
+def modality_objectively : Modality (MonPred I PROP) (MonPred I PROP) where
+  M := MonPred.objectively
+  action _ := .transform fun P Q => Objective P ∧ P = Q
+  spec := by
+    intro p P Q h
+    have hPQ : P = Q := h.2
+    subst hPQ
+    haveI := h.1
+    exact objective_objectively iprop(□?p P)
+  emp := monPred_objectively_emp.mpr
+  mono := monPred_objectively_mono
+  sep := monPred_objectively_sep_2 _ _
